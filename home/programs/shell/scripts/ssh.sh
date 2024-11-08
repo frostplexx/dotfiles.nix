ed28ce736bfc39e747bd7c7ed6f2bcbaedcc71e0
daniel <daniel.inama02@gmail.com>
2024-11-08 15:55:11 +0100

    feat: update ssh scipt to pull all info from 1password

--- home/programs/shell/scripts/ssh.sh
+++ home/programs/shell/scripts/ssh.sh
@@ -1,34 +1,36 @@
 #!/usr/bin/env bash
-# Helper function to get 1Password SSH password
-get_ssh_password() {
-  local host=$1
-  # Remove both 'setenv' and 'ONEPASSWORD_ITEM=' prefixes
-  local op_item=$(ssh -G "$host" | grep "^setenv ONEPASSWORD_ITEM" | sed 's/^setenv ONEPASSWORD_ITEM=//')
-  local password=$(op item get "$op_item" --format json 2>/dev/null | jq -r '.fields[] | select(.label == "password") .value | select(. != null) | values' | head -n1)
-  # if password is empty, run notify kitten and return empty string
-  if [[ -z $password ]]; then
-    kitten notify "No password found for $host"
-  fi
-  echo "$password"
-}
-
 if [[ $# -eq 0 ]]; then
-  local selected_host=$(cat ~/.ssh/config ~/.orbstack/ssh/config 2>/dev/null | grep "^Host " | cut -d ' ' -f 2- | tr ' ' '\n' | grep -v '^*$' | sort -u | fzf --height 40% --reverse)
-  if [[ -n $selected_host ]]; then
-    local password=$(get_ssh_password "$selected_host")
+  # Get all items with ssh tag and format them for fzf
+  selected_entry=$(op item list --tags ssh --format json | \
+    jq -r '.[] | [.title, .id] | @tsv' | \
+    fzf --height 40% --reverse --with-nth=1 --delimiter=$'\t')
+
+  if [[ -n $selected_entry ]]; then
+    # Extract the item ID using tab as delimiter
+    item_id=$(echo "$selected_entry" | cut -f2)
+
+    # Get full item details
+    item_details=$(op item get "$item_id" --format json)
+
+    # Extract hostname, username, and password
+    hostname=$(echo "$item_details" | jq -r '.fields[] | select(.label == "hostname") .value | select(. != null) | values' | head -n1)
+    username=$(echo "$item_details" | jq -r '.fields[] | select(.label == "username") .value | select(. != null) | values' | head -n1)
+    password=$(echo "$item_details" | jq -r '.fields[] | select(.label == "password") .value | select(. != null) | values' | head -n1)
+
+    if [[ -z $hostname ]]; then
+      # If no hostname field, use the title as hostname
+      hostname=$(echo "$item_details" | jq -r '.title')
+    fi
+
     if [[ -n $password ]]; then
-      echo "Connecting to $selected_host..."
-      sshpass -p "$password" kitten ssh "$selected_host"
+      clear
+      echo "Connecting to $hostname as $username..."
+      sshpass -p "$password" kitten ssh -l "$username" "$hostname"
     else
-      kitten ssh "$selected_host"
+      kitten ssh -l "$username" "$hostname"
     fi
   fi
 else
-  password=$(get_ssh_password "$1")
-  if [[ -n $password ]]; then
-    echo "Connecting to $1..."
-    sshpass -p "$password" kitten ssh "$@"
-  else
-    kitten ssh "$@"
-  fi
+  # If arguments provided, use traditional SSH
+  kitten ssh "$@"
 fi
--- home/programs/ssh/default.nix
+++ home/programs/ssh/default.nix
@@ -1,4 +1,3 @@
-{ ... }:
 {
   programs.ssh = {
     enable = true;
@@ -9,46 +8,5 @@
       Include ~/.orbstack/ssh/config
       IdentityAgent = "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
     '';
-    # Host-specific configurations
-    matchBlocks = {
-
-      "NixOS" = {
-        user = "daniel";
-        hostname = "192.168.1.101";
-         extraOptions = {
-          "SetEnv" = "ONEPASSWORD_ITEM=\"Desktop PC\"";
-        };
-      };
-
-      "CIP" = {
-        hostname = "remote.cip.ifi.lmu.de";
-        user = "inama";
-         extraOptions = {
-          "SetEnv" = "ONEPASSWORD_ITEM=\"LMU CIP\"";
-        };
-      };
-
-      "Hetzner_VPS" = {
-        hostname = "37.27.26.175";
-        user = "ubuntu";
-         extraOptions = {
-          "SetEnv" = "ONEPASSWORD_ITEM=\"Hetzner VPS\"";
-        };
-      };
-
-      "Proxmox" = {
-          hostname = "192.168.1.85";
-          user = "root";
-         extraOptions = {
-          "SetEnv" = "ONEPASSWORD_ITEM=Proxmox";
-        };
-      };
-
-      "MetisBot" = {
-        hostname = "129.152.13.213";
-        user = "ubuntu";
-      };
-
-    };
   };
 }
