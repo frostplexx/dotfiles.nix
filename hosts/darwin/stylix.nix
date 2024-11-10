2ccf74ea4110e87097b9b7b3fbf9210f823c97ad
daniel <daniel.inama02@gmail.com>
2024-11-10 20:29:21 +0100

    feat(editor): add language servers pyright, gopls, and ruff for enhanced development support fix(shell): correct temporary directory creation for yazi script to ensure proper cleanup chore(shell): format configuration for better readability and maintainability refactor(shell): improve yazi script to handle temporary directory and file more effectively chore(stylix): remove yazi target enablement from darwin and nixos configurations as it is no longer needed

--- hosts/darwin/stylix.nix
+++ hosts/darwin/stylix.nix
@@ -6,6 +6,5 @@
     image = ../../../home/programs/plasma/wallpaper.jpg;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
     opacity.terminal = 0.9;
-targets.yazi.enable = true;
   };
 }
