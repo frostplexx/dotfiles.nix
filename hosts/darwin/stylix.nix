b26da7174f17fb8f07e57b3b8872eefb72e6e8b2
daniel <daniel.inama02@gmail.com>
2024-11-14 19:56:19 +0100

    [darwin] Gen:  Generation 239 - 2024-11-14 at 19:55
    Changed files:
    discord_krisp.log
    discord_utils.log
    home/programs/aerospace/aerospace.toml
    hosts/darwin/stylix.nix
    
    Summary:
     discord_krisp.log (new)                | 8 ++++++++
     discord_utils.log (new)                | 1 +
     home/programs/aerospace/aerospace.toml | 4 ++++
     hosts/darwin/stylix.nix                | 3 ++-
     4 files changed, 15 insertions(+), 1 deletion(-)

--- hosts/darwin/stylix.nix
+++ hosts/darwin/stylix.nix
@@ -4,7 +4,8 @@
   stylix = {
     enable = true;
     image = ../../../home/programs/plasma/wallpaper.jpg;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
     opacity.terminal = 0.9;
   };
 }
