aebbae8e7babed5214eaa774c10074ce72ef58fc
daniel <daniel.inama02@gmail.com>
2024-11-14 19:58:28 +0100

    [darwin] Gen:  Generation 240 - 2024-11-14 at 19:57
    Changed files:
    home/programs/editor/nvim/lua/plugins/catppuccin.lua
    hosts/darwin/stylix.nix
    
    Summary:
     .../editor/nvim/lua/plugins/catppuccin.lua         | 80 +++++++++++-----------
     hosts/darwin/stylix.nix                            |  3 +-
     2 files changed, 41 insertions(+), 42 deletions(-)

--- hosts/darwin/stylix.nix
+++ hosts/darwin/stylix.nix
@@ -4,8 +4,7 @@
   stylix = {
     enable = true;
     image = ../../../home/programs/plasma/wallpaper.jpg;
-    # base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
     opacity.terminal = 0.9;
   };
 }
