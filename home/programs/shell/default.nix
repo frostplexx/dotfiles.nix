b5f7a9f9d6a0837e54eaae6420762aae3d39b636
daniel <daniel.inama02@gmail.com>
2024-11-19 23:40:16 +0100

    [darwin] Gen:  285 - 2024-11-19 at 23:39
    Changed files:
    home/programs/shell/default.nix
    
    Summary:
     home/programs/shell/default.nix | 18 +-----------------
     1 file changed, 1 insertion(+), 17 deletions(-)

--- home/programs/shell/default.nix
+++ home/programs/shell/default.nix
@@ -102,24 +102,8 @@
           sort_reverse = false;
           sort_dir_first = true;
           linemode = "mtime";
-          show_hidden = true;
+          show_hidden = false;
           show_symlink = true;
-        };
-
-        preview = {
-          image_filter = "lanczos3";
-          image_quality = 90;
-          tab_size = 1;
-          max_width = 600;
-          max_height = 900;
-          cache_dir = "";
-          ueberzug_scale = 1;
-          ueberzug_offset = [
-            0
-            0
-            0
-            0
-          ];
         };
 
         tasks = {
