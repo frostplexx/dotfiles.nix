e5a736dc8d9a46b1c47b9d48e26ca1536a1bd831
daniel <daniel.inama02@gmail.com>
2024-11-20 14:12:52 +0100

    [darwin] Gen:  295 - 2024-11-20 at 14:12
    Changed files:
    flake.lock
    home/programs/spicetify/default.nix
    
    Summary:
     flake.lock                          | 48 ++++++++++++++++++-------------------
     home/programs/spicetify/default.nix | 23 +-----------------
     2 files changed, 25 insertions(+), 46 deletions(-)

--- home/programs/spicetify/default.nix
+++ home/programs/spicetify/default.nix
@@ -15,28 +15,7 @@
     ];
 
     # Pull the theme directly from the spicetify-themes repo because the nixpkgs is a bit out of date
-    theme = {
-      name = "text";
-      src = pkgs.fetchFromGitHub {
-        owner = "spicetify";
-        repo = "spicetify-themes";
-        rev = "c6e82dfeaa46ee9060d0c02fc437989eb77f6c61";
-        sha256 = "kpHIWHuubTEwIoi+645Ai/PqXTlZMhRcBueYwgCqG2E=";
-        postFetch = ''
-          tar xf $downloadedFile --strip=2 "*/text"
-        '';
-      };
-      patches = {
-        "xpui.js_find_8008" = ",(\\w+=)56";
-        "xpui.js_repl_8008" = ",\${1}32";
-      };
-       injectCss = true;
-  injectThemeJs = true;
-  replaceColors = true;
-  homeConfig = true;
-  overwriteAssets = false;
-  additonalCss = "";
-    };
+    theme = spicePkgs.themes.text;
     colorScheme = "CatppuccinMocha";
   };
 }
