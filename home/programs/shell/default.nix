055432d7c7be151d12b015bf6c0a93645fa87d20
daniel <daniel.inama02@gmail.com>
2024-11-20 10:20:12 +0100

    [darwin] Gen:  289 - 2024-11-20 at 10:19
    Changed files:
    home/programs/shell/default.nix
    
    Summary:
     home/programs/shell/default.nix | 5 ++++-
     1 file changed, 4 insertions(+), 1 deletion(-)

--- home/programs/shell/default.nix
+++ home/programs/shell/default.nix
@@ -147,7 +147,10 @@ in {
       };
 
       initLua = ''
-        require("full-border"):setup()
+        require("full-border"):setup {
+        	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+        	type = ui.Border.ROUNDED,
+        }
         require("starship"):setup()
         require("git"):setup()
       '';
