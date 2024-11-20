9dc470b3b4c887e3a911e0df391b30eef9682c87
daniel <daniel.inama02@gmail.com>
2024-11-20 13:21:54 +0100

    [darwin] Gen:  290 - 2024-11-20 at 13:21
    Changed files:
    home/programs/spicetify/default.nix
    
    Summary:
     home/programs/spicetify/default.nix | 24 ++++++++++++++++++++++--
     1 file changed, 22 insertions(+), 2 deletions(-)

--- home/programs/spicetify/default.nix
+++ home/programs/spicetify/default.nix
@@ -13,7 +13,27 @@
       playlistIcons
       goToSong
     ];
-    theme = spicePkgs.themes.text;
-    colorScheme = "CatppuccinMocha";
+
+    # Pull the theme directly from the spicetify-themes repo because the nixpkgs is a bit out of date
+    theme = {
+      name = "text";
+      src = pkgs.fetchFromGitHub {
+        owner = "spicetify";
+        repo = "spicetify-themes";
+        rev = "master";
+        hash = "sha256-kpHIWHuubTEwIoi+645Ai/PqXTlZMhRcBueYwgCqG2E=";
+      };
+
+      # Additional theme options all set to defaults
+      # the docs of the theme should say which of these
+      # if any you have to change
+      injectCss = true;
+      injectThemeJs = true;
+      replaceColors = true;
+      homeConfig = true;
+      overwriteAssets = false;
+      additonalCss = "";
+    };
+    # colorScheme = "CatppuccinMocha";
   };
 }
