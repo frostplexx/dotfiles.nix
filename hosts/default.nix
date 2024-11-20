b8d146df941467a6f7c4699c95200b077d22f3e7
daniel <daniel.inama02@gmail.com>
2024-11-20 20:53:42 +0100

    feat: more chanfes

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,6 +1,75 @@
 _:
 
 {
+
+  # Your existing nixosConfigurations and darwinConfigurations stay the same
+  nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    specialArgs = {inherit vars inputs;};
+    modules = [
+      ./nix/core.nix
+      inputs.yuki.nixosModules.default
+      ./hosts/nixos/configuration.nix
+      inputs.stylix.nixosModules.stylix
+      home-manager.nixosModules.home-manager
+      {
+        nixpkgs.overlays = [inputs.nur.overlay];
+        home-manager = {
+          useGlobalPkgs = true;
+          useUserPackages = true;
+          extraSpecialArgs = {inherit vars inputs;};
+          users.${vars.user} = import ./home;
+          sharedModules = [
+            inputs.plasma-manager.homeManagerModules.plasma-manager
+            inputs.nixcord.homeManagerModules.nixcord
+            inputs.spicetify-nix.homeManagerModules.default
+            {
+              # explicitly disable stylix for spicetify because its managed by spicetify-nix
+              # TODO: This is a stupid place to put this and needs to be refactored
+              stylix.targets.spicetify.enable = false;
+            }
+          ];
+        };
+      }
+    ];
+  };
+
+
+  darwinConfigurations.darwin = nix-darwin.lib.darwinSystem {
+    system = "aarch64-darwin";
+    specialArgs = {inherit vars inputs;};
+    modules = [
+      ./nix/core.nix
+      inputs.yuki.nixosModules.default
+      inputs.stylix.darwinModules.stylix
+      ./hosts/darwin/configuration.nix
+      inputs.darwin-custom-icons.darwinModules.default
+      home-manager.darwinModules.home-manager
+      {
+        nixpkgs.overlays = [
+          inputs.nixpkgs-firefox-darwin.overlay
+          inputs.nur.overlay
+        ];
+        home-manager = {
+          useGlobalPkgs = true;
+          useUserPackages = true;
+          extraSpecialArgs = {inherit vars inputs;};
+          users.${vars.user} = import ./home;
+          sharedModules = [
+            inputs.plasma-manager.homeManagerModules.plasma-manager
+            inputs.nixcord.homeManagerModules.nixcord
+            inputs.spicetify-nix.homeManagerModules.default
+            {
+              # explicitly disable stylix for spicetify because its managed by spicetify-nix
+              # TODO: This is a stupid place to put this and needs to be refactored
+              stylix.targets.spicetify.enable = false;
+            }
+          ];
+        };
+      }
+    ];
+  };
+
   # nix-darwin configurations
   parts.darwinConfigurations = {
 lyra = {
