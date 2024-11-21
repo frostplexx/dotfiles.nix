f767409ab0d506df78cbddca56e37253da902657
daniel <daniel.inama02@gmail.com>
2024-11-21 15:25:20 +0100

    [nixos] Gen:
    Changed files:
    hosts/machines/phoenix/configuration.nix
    
    Summary:
     hosts/machines/phoenix/configuration.nix | 2 +-
     1 file changed, 1 insertion(+), 1 deletion(-)

--- hosts/machines/phoenix/configuration.nix
+++ hosts/machines/phoenix/configuration.nix
@@ -156,7 +156,7 @@
       };
       open = false;
       nvidiaSettings = true;
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      package = config.boot.kernelPackages.nvidiaPackages.beta;
     };
   };
 
