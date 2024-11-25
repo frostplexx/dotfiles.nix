0bb094ecfc6d6cea04f25f905448c8c521db1a60
daniel <daniel.inama02@gmail.com>
2024-11-25 10:39:41 +0100

    feat: add obsidian.nvim

--- home/programs/editor/nvim/lua/plugins/copilot.lua
+++ home/programs/editor/nvim/lua/plugins/copilot.lua
@@ -50,21 +50,4 @@ return {
             })
         end,
     },
-    {
-        "CopilotC-Nvim/CopilotChat.nvim",
-        branch = "canary",
-        dependencies = {
-            { "zbirenbaum/copilot.lua" }, -- for curl, log wrapper
-            { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-        },
-        build = "make tiktoken",    -- Only on MacOS or Linux
-        opts = {
-            -- See Configuration section for options
-        },
-
-        keys = {
-            { "<leader>ca", ":CopilotChatToggle<CR>", desc = "Toggle Copilot Chat", silent = true }
-        }
-        -- See Commands section for default commands if you want to lazy load on them
-    },
 }
