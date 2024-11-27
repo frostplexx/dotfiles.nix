203dece6465f1aca4a2644095db5ce18fc3137d9
daniel <daniel.inama02@gmail.com>
2024-11-27 12:55:13 +0100

    add steam option

--- hosts/machines/phoenix/sunshine.nix
+++ hosts/machines/phoenix/sunshine.nix
@@ -11,6 +11,10 @@
     export DISPLAY=:0
     ${pkgs.xorg.xrandr}/bin/xrandr --output DP-2 --mode 2560x1080 --rate 144
   '';
+  steamImage = builtins.fetchurl {
+    url = "https://cdn2.steamgriddb.com/grid/39c2966989c4f0091a99eef7f1d09c09.png";
+    sha256 = "sha256:18hka29nfwxj5xzfhxhy2ccjjaxhb8ir6v7wx0rs2lwc941r36b1";
+  };
 in {
   services = {
     sunshine = {
@@ -39,6 +43,20 @@ in {
             ];
             exclude-global-prep-cmd = "false";
             auto-detach = "true";
+          }
+          {
+            name = "Steam Big Picture";
+            image-path = "${steamImage}";
+            cmd = "${pkgs.steam}/bin/steam -bigpicture";
+            prep-cmd = [
+              {
+                do = "${connectionScript}/bin/script";
+                undo = "${disconnectionScript}/bin/script";
+              }
+            ];
+            exclude-global-prep-cmd = "false";
+            auto-detach = "true";
+            detached = ["${pkgs.steam}/bin/steam"];
           }
         ];
       };
