b52f3fd9420ba2d444b0fa005234eed5c2a0af7d
daniel <daniel.inama02@gmail.com>
2024-11-28 11:12:07 +0100

    add lualine

--- home/programs/editor/nvim/lua/plugins/latex.lua
+++ home/programs/editor/nvim/lua/plugins/latex.lua
@@ -1,6 +1,31 @@
 return {
-	"lervag/vimtex",
-	lazy = true,
-	enabled = true,
-	event = "BufRead *.tex",
+    {
+        "lervag/vimtex",
+        lazy = true,
+        enabled = true,
+        event = "BufRead *.tex",
+        ft = { "tex" },
+    },
+    {
+        "barreiroleo/ltex_extra.nvim",
+        lazy = true,
+        event = "BufRead *.tex",
+        enabled = true,
+        ft = { "tex" },
+        dependencies = { "neovim/nvim-lspconfig" },
+        -- yes, you can use the opts field, just I'm showing the setup explicitly
+        config = function()
+            require("ltex_extra").setup({
+                server_opts = {
+                    --  function(client, buffer)
+                    on_attach = function(_, _)
+                        -- your on_attach process
+                    end,
+                    settings = {
+                        ltex = {},
+                    },
+                },
+            })
+        end,
+    }
 }
