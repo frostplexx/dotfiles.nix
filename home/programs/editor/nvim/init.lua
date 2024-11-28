18d8fdd290e7cc3299e3f0fb8348a9185bebae22
daniel <daniel.inama02@gmail.com>
2024-11-28 11:00:40 +0100

    fjsdkal;

--- home/programs/editor/nvim/init.lua
+++ home/programs/editor/nvim/init.lua
@@ -2,55 +2,57 @@
 vim.loader.enable() -- speed up startup time
 
 require("keymap")   -- load keymaps
+require('rename-ui').setup()
+require('auto-pairs').setup()
 
 -- [[ Lazy.nvim Plugin Manager ]]
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 
 vim.opt.rtp:prepend(lazypath)
 -- initialize lazy.nvim
 require("lazy").setup({
-  spec = {
-    { import = "plugins" },
-  },
-  dev = {
-    path = "~/.local/share/nvim/nix",
-    fallback = false,
-  },
-  defaults = {
-    lazy = true,
-    version = false,             -- always use the latest git commit
-  },
-  checker = { enabled = false }, -- automatically check for plugin updates
-  ui = {
-    border = "rounded",
-  },
-  performance = {
-    cache = {
-      enabled = true,
+    spec = {
+        { import = "plugins" },
     },
-    rtp = {
-      reset = true, -- reset runtimepath
-      -- disable some rtp plugins
-      disabled_plugins = {
-        "gzip",
-        "matchit",
-        "matchparen",
-        "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
+    dev = {
+        path = "~/.local/share/nvim/nix",
+        fallback = false,
     },
-  },
+    defaults = {
+        lazy = true,
+        version = false,         -- always use the latest git commit
+    },
+    checker = { enabled = false }, -- automatically check for plugin updates
+    ui = {
+        border = "rounded",
+    },
+    performance = {
+        cache = {
+            enabled = true,
+        },
+        rtp = {
+            reset = true, -- reset runtimepath
+            -- disable some rtp plugins
+            disabled_plugins = {
+                "gzip",
+                "matchit",
+                "matchparen",
+                "netrwPlugin",
+                "tarPlugin",
+                "tohtml",
+                "tutor",
+                "zipPlugin",
+            },
+        },
+    },
 })
