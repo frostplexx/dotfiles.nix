9c0561299c4b2dff930827ce4136af49b25ea5e6
daniel <daniel.inama02@gmail.com>
2024-11-28 10:34:58 +0100

    remove unused plugins from nvim config

--- home/programs/editor/nvim/lua/plugins/mini-nvim.lua
+++ home/programs/editor/nvim/lua/plugins/mini-nvim.lua
@@ -5,99 +5,6 @@ return {
     event = "BufReadPost",
     enabled = true,
     config = function()
-        -- [[ Gitsigns ]]]
-
-        -- [[ Mini Files ]]
-        -- require("mini.files").setup({
-        --   -- Customization of shown content
-        --   content = {
-        --     -- Predicate for which file system entries to show
-        --     filter = nil,
-        --     -- What prefix to show to the left of file system entry
-        --     prefix = nil,
-        --     -- In which order to show file system entries
-        --     sort = nil,
-        --   },
-        --
-        --   -- Module mappings created only inside explorer.
-        --   -- Use `''` (empty string) to not create one.
-        --   mappings = {
-        --     close       = 'q',
-        --     go_in       = 'l',
-        --     go_in_plus  = '<cr>',
-        --     go_out      = 'h',
-        --     go_out_plus = 'H',
-        --     mark_goto   = "'",
-        --     mark_set    = 'm',
-        --     reset       = '<BS>',
-        --     reveal_cwd  = '@',
-        --     show_help   = 'g?',
-        --     synchronize = '=',
-        --     trim_left   = '<',
-        --     trim_right  = '>',
-        --   },
-        --
-        --   -- General options
-        --   options = {
-        --     -- Whether to delete permanently or move into module-specific trash
-        --     permanent_delete = false,
-        --     -- Whether to use for editing directories
-        --     use_as_default_explorer = true,
-        --   },
-        --
-        --   -- Customization of explorer windows
-        --   windows = {
-        --     -- Maximum number of windows to show side by side
-        --     max_number = math.huge,
-        --     -- Whether to show preview of file/directory under cursor
-        --     preview = true,
-        --     -- Width of focused window
-        --     width_focus = 50,
-        --     -- Width of non-focused window
-        --     width_nofocus = 15,
-        --     -- Width of preview window
-        --     width_preview = 25,
-        --   },
-        -- })
-
-        -- [[ Mini Indetenscope ]]
-        -- require("mini.indentscope").setup({
-        --     -- Draw options
-        --     draw = {
-        --         -- Delay (in ms) between event and start of drawing scope indicator
-        --         delay = 0,
-        --         animation = require("mini.indentscope").gen_animation.none(),
-        --     },
-        --
-        --     -- Module mappings. Use `''` (empty string) to disable one.
-        --     mappings = {
-        --         -- Textobjects
-        --         object_scope = "ii",
-        --         object_scope_with_border = "ai",
-        --
-        --         -- Motions (jump to respective border line; if not present - body line)
-        --         goto_top = "[i",
-        --         goto_bottom = "]i",
-        --     },
-        --
-        --     -- Options which control scope computation
-        --     options = {
-        --         border = "both",
-        --         indent_at_cursor = true,
-        --         try_as_border = false,
-        --     },
-        --     symbol = "│",
-        -- })
-
-        -- [[ Mini Pairs ]]
-        require("mini.pairs").setup({
-            disablee_in_macro = false,
-            map_cr = false,
-        })
-
-        -- [[ Mini Surround ]]
-        require("mini.surround").setup()
-
         local statusline = function()
             local mini = require("mini.statusline")
 
@@ -159,7 +66,4 @@ return {
             }
         )
     end,
-    -- keys = {
-    --   { "<leader>e", ":lua MiniFiles.open()<cr>", desc = "Mini Files" }
-    -- }
 }
