8f1865ff0f40b5c681f37860e4e8405279c79884
daniel <daniel.inama02@gmail.com>
2024-11-30 14:01:08 +0100

    remove some custom icons

--- hosts/machines/lyra/custom_icons/custom_icons.nix
+++ hosts/machines/lyra/custom_icons/custom_icons.nix
@@ -14,14 +14,14 @@
         path = "${pkgs.kitty}/Applications/kitty.app";
         icon = ../../../../home/programs/kitty/kitty.app.png;
       }
-      {
-        path = "${config.users.users.daniel.home}/Applications/Home Manager Apps/Firefox.app";
-        icon = ./firefox.icns;
-      }
-      {
-        path = "/Applications/Things3.app";
-        icon = ./things.icns;
-      }
+      # {
+      #   path = "${config.users.users.daniel.home}/Applications/Home Manager Apps/Firefox.app";
+      #   icon = ./firefox.icns;
+      # }
+      # {
+      #   path = "/Applications/Things3.app";
+      #   icon = ./things.icns;
+      # }
       {
         path = "${pkgs.vesktop}/Applications/Vesktop.app";
         icon = ./discord.icns;
