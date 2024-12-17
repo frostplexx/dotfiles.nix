3e95f0b0c73928f2ebc87a03aac85c192aae0351
daniel <daniel.inama02@gmail.com>
2024-12-17 23:04:30 +0100

    switch to zen browser

--- flake.nix
+++ flake.nix
@@ -44,7 +44,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     # zen-browser.url = "git+file:///Users/daniel/Developer/zen-browser-flake";
-    zen-browser.url = "github:frostplexx/zen-browser-flake";
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
   };
 
   outputs = inputs: let
