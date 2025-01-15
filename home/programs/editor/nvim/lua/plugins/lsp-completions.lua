f3a798ed26afe110be8797803d6113a3f92cd1f0
daniel <daniel.inama02@gmail.com>
2025-01-15 10:38:35 +0100

    feat: switch fzf-lua for snacks, enable copilot again

--- home/programs/editor/nvim/lua/plugins/lsp-completions.lua
+++ home/programs/editor/nvim/lua/plugins/lsp-completions.lua
@@ -5,9 +5,80 @@ return {
         event = "InsertEnter",
         dependencies = {
             "rafamadriz/friendly-snippets",
+            "moyiz/blink-emoji.nvim",
+            "giuxtaposition/blink-cmp-copilot",
         },
         version = "v0.*",
         opts = {
+            sources = {
+                default = {
+                    'lsp',
+                    'path',
+                    'snippets',
+                    'buffer',
+                    "copilot",
+                    "emoji",
+                },
+                providers = {
+                    copilot = {
+                        name = "copilot",
+                        module = "blink-cmp-copilot",
+                        score_offset = 100,
+                        async = true,
+                        transform_items = function(_, items)
+                            local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+                            local kind_idx = #CompletionItemKind + 1
+                            CompletionItemKind[kind_idx] = "Copilot"
+                            for _, item in ipairs(items) do
+                                item.kind = kind_idx
+                            end
+                            return items
+                        end,
+                    },
+                    emoji = {
+                        module = "blink-emoji",
+                        name = "Emoji",
+                        score_offset = 15,        -- Tune by preference
+                        opts = { insert = true }, -- Insert emoji (default) or complete its name
+                    }
+                }
+            },
+            appearance = {
+                -- Blink does not expose its default kind icons so you must copy them all (or set your custom ones) and add Copilot
+                kind_icons = {
+                    Copilot = '',
+                    Text = '󰉿',
+                    Method = '󰊕',
+                    Function = '󰊕',
+                    Constructor = '󰒓',
+
+                    Field = '󰜢',
+                    Variable = '󰆦',
+                    Property = '󰖷',
+
+                    Class = '󱡠',
+                    Interface = '󱡠',
+                    Struct = '󱡠',
+                    Module = '󰅩',
+
+                    Unit = '󰪚',
+                    Value = '󰦨',
+                    Enum = '󰦨',
+                    EnumMember = '󰦨',
+
+                    Keyword = '󰻾',
+                    Constant = '󰏿',
+
+                    Snippet = '󱄽',
+                    Color = '󰏘',
+                    File = '󰈔',
+                    Reference = '󰬲',
+                    Folder = '󰉋',
+                    Event = '󱐋',
+                    Operator = '󰪚',
+                    TypeParameter = '󰬛',
+                },
+            },
             keymap = { preset = 'super-tab' },
             completion = {
                 menu = { border = 'single' },
--- home/programs/editor/nvim/lua/plugins/snacks.lua
+++ home/programs/editor/nvim/lua/plugins/snacks.lua
@@ -3,30 +3,73 @@ return {
     priority = 1000,
     lazy = false,
     opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-        bigfile = { enabled = true },
+        bigfile = {
+            enabled = true,
+            notify = true,
+            size = 100 * 1024 -- 100Kb
+        },
+        animate = {
+            enabled = true,
+            duration = 20, -- ms per step
+            easing = "linear",
+            fps = 144,     -- frames per second. Global setting for all animations
+        },
         debug = { enabled = true },
         input = { enabled = true },
         notifier = { enabled = true },
         quickfile = { enabled = true },
-        statuscolumn = { enabled = true },
+        statuscolumn = {
+            enabled = true,
+            left = { "mark", "sign" }, -- priority of signs on the left (high to low)
+            right = { "fold", "git" }, -- priority of signs on the right (high to low)
+            folds = {
+                open = false,          -- show open fold icons
+                git_hl = false,        -- use Git Signs hl for fold icons
+            },
+            git = {
+                -- patterns to match Git signs
+                patterns = { "GitSign", "MiniDiffSign" },
+            },
+            refresh = 50, -- refresh at most every 50ms
+        },
         scroll = { enabled = false },
         words = { enabled = true },
+
+        picker = {
+            win = {
+                -- input window
+                input = {
+                    keys = {
+                        ["<Esc>"] = { "close", mode = { "n", "i" } },
+                    },
+                },
+            },
+            reverse = true,
+        }
     },
     keys = {
-        { "<leader>z",  function() Snacks.zen() end,                     desc = "Toggle Zen Mode" },
-        { "<leader>.",  function() Snacks.scratch() end,                 desc = "Toggle Scratch Buffer" },
-        { "<leader>S",  function() Snacks.scratch.select() end,          desc = "Select Scratch Buffer" },
-        { "<leader>n",  function() Snacks.notifier.show_history() end,   desc = "Notification History" },
-        { "<leader>bd", function() Snacks.bufdelete() end,               desc = "Delete Buffer" },
-        { "<leader>cR", function() Snacks.rename.rename_file() end,      desc = "Rename File" },
-        { "<leader>gf", function() Snacks.lazygit.log_file() end,        desc = "Lazygit Current File History" },
-        { "<leader>gg", function() Snacks.lazygit() end,                 desc = "Lazygit" },
-        { "<leader>gl", function() Snacks.lazygit.log() end,             desc = "Lazygit Log (cwd)" },
-        { "<leader>un", function() Snacks.notifier.hide() end,           desc = "Dismiss All Notifications" },
-        { "]]",         function() Snacks.words.jump(vim.v.count1) end,  desc = "Next Reference",              mode = { "n", "t" } },
-        { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference",              mode = { "n", "t" } },
+        { "<leader>z",       function() Snacks.zen() end,                     desc = "Toggle Zen Mode" },
+        { "<leader>.",       function() Snacks.scratch() end,                 desc = "Toggle Scratch Buffer" },
+        { "<leader>S",       function() Snacks.scratch.select() end,          desc = "Select Scratch Buffer" },
+        { "<leader>n",       function() Snacks.notifier.show_history() end,   desc = "Notification History" },
+        { "<leader>bd",      function() Snacks.bufdelete() end,               desc = "Delete Buffer" },
+        { "<leader>cR",      function() Snacks.rename.rename_file() end,      desc = "Rename File" },
+        { "<leader>gf",      function() Snacks.lazygit.log_file() end,        desc = "Lazygit Current File History" },
+        { "<leader>gg",      function() Snacks.lazygit() end,                 desc = "Lazygit" },
+        { "<leader>gl",      function() Snacks.lazygit.log() end,             desc = "Lazygit Log (cwd)" },
+        { "<leader>go",      function() Snacks.gitbrowse() end,               desc = "Open Current Repo in Browser" },
+        { "<leader>un",      function() Snacks.notifier.hide() end,           desc = "Dismiss All Notifications" },
+        { "]]",              function() Snacks.words.jump(vim.v.count1) end,  desc = "Next Reference",              mode = { "n", "t" } },
+        { "[[",              function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference",              mode = { "n", "t" } },
+
+        { "<leader><space>", function() Snacks.picker.files() end,            desc = "Find Files",                  remap = true,       silent = true },
+        { "<leader>bf",      function() Snacks.picker.buffers() end,          desc = "List open Buffers",           remap = true,       silent = true },
+        { "<leader>ch",      function() Snacks.picker.command_history() end,  desc = "Command History",             silent = true },
+        { "<leader>km",      function() Snacks.picker.keymaps() end,          desc = "Keymap",                      silent = true },
+        { "<leader>fg",      function() Snacks.picker.grep() end,             desc = "Live Grep",                   silent = true },
+        { "<leader>fh",      function() Snacks.picker.help() end,             desc = "Help Tags",                   silent = true },
+        { "<leader>ss",      function() Snacks.picker.lsp_symbols() end,      desc = "LSP Symbols" },
+
+
     },
 }
