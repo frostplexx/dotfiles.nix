ce62ad058a94c8441ec680aa8d7fb01840452d46
daniel <daniel.inama02@gmail.com>
2025-01-16 14:24:22 +0100

    stuff

--- home/programs/kitty/default.nix
+++ home/programs/kitty/default.nix
@@ -34,7 +34,7 @@
       cursor_shape = "block";
       cursor_blink_interval = "-1";
       cursor_stop_blinking_after = "30.0";
-      cursor_trail = "0";
+      cursor_trail = "3";
 
       # Scrolling
       scrollback_lines = "10000";
@@ -63,7 +63,7 @@
       tab_bar_edge = "top";
       tab_bar_style = "powerline";
       tab_powerline_style = "slanted";
-      tab_title_template = "{index} { tab.active_exe if tab.active_exe not in ('-zsh', 'kitten') else title}";
+      tab_title_template = "{index} { tab.active_exe if tab.active_exe not in ('-zsh', 'kitten') else title}{tab.last_focused_progress_percent}";
       active_tab_font_style = "bold";
       inactive_tab_font_style = "normal";
       active_tab_background = "#8aadf4";
