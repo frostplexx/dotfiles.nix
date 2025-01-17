b3556bec861d7626ad6a9ceb5d34ec3b7a7d38fa
daniel <daniel.inama02@gmail.com>
2025-01-17 11:57:25 +0100

    update to beta gpu drivers

--- hosts/machines/phoenix/configuration.nix
+++ hosts/machines/phoenix/configuration.nix
@@ -167,7 +167,7 @@
       };
       open = false;
       nvidiaSettings = true;
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      package = config.boot.kernelPackages.nvidiaPackages.beta;
     };
   };
 
