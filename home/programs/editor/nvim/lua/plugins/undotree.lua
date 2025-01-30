efb98c794544464929254572a87688689b3d6634
daniel <daniel.inama02@gmail.com>
2025-01-30 23:16:47 +0100

    changes

--- home/programs/editor/nvim/lua/plugins/undotree.lua
+++ home/programs/editor/nvim/lua/plugins/undotree.lua
@@ -1,6 +1,7 @@
 return {
     "mbbill/undotree",
     lazy = true,
+    enabled = false,
     keys = {
         { "<leader>cu", ":UndotreeToggle<CR>", desc = "Toggle undotree", silent = true }
     }
