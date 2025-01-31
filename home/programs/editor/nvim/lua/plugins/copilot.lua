21f365627695177db0d139bcc3d2e0499fa3068c
daniel <daniel.inama02@gmail.com>
2025-01-31 11:59:08 +0100

    fixes

--- home/programs/editor/nvim/lua/plugins/copilot.lua
+++ home/programs/editor/nvim/lua/plugins/copilot.lua
@@ -1,14 +1,40 @@
 return {
     {
-        "zbirenbaum/copilot.lua",
-        cmd = "Copilot",
-        event = "InsertEnter",
-        enabled = true,
-        config = function()
-            require("copilot").setup({
-                suggestion = { enabled = false },
-                panel = { enabled = false },
-            })
-        end,
-    },
+        "yetone/avante.nvim",
+        event = "VeryLazy",
+        lazy = false,
+        version = false,
+        build = "make",
+        dependencies = {
+            "stevearc/dressing.nvim",
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+        },
+        opts = {
+            provider = "openai",
+            auto_suggestions_provider = "openai", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
+            -- openai = {
+            --     endpoint = "https://api.deepseek.com/v1",
+            --     model = "deepseek-chat",
+            --     timeout = 30000, -- Timeout in milliseconds
+            --     temperature = 0,
+            --     max_tokens = 4096,
+            --     -- optional
+            --     api_key_name = "OPENAI_API_KEY", -- default OPENAI_API_KEY if not set
+            -- },
+            hints = { enabled = false },
+        },
+    }
+    -- {
+    --     "zbirenbaum/copilot.lua",
+    --     cmd = "Copilot",
+    --     event = "InsertEnter",
+    --     enabled = true,
+    --     config = function()
+    --         require("copilot").setup({
+    --             suggestion = { enabled = false },
+    --             panel = { enabled = false },
+    --         })
+    --     end,
+    -- },
 }
--- home/programs/editor/nvim/lua/plugins/lsp-server.lua
+++ home/programs/editor/nvim/lua/plugins/lsp-server.lua
@@ -89,7 +89,19 @@ return {
                     capabilities = require('blink.cmp').get_lsp_capabilities(),
                 },
                 excluded_servers = {
-                    "ccls", "zk",
+                    "ccls",            -- prefer clangd
+                    "denols",          -- prefer eslint and ts_ls
+                    "docker_compose_language_service", -- yamlls should be enough?
+                    "flow",            -- prefer eslint and ts_ls
+                    "ltex",            -- grammar tool using too much CPU
+                    "quick_lint_js",   -- prefer eslint and ts_ls
+                    "scry",            -- archived on Jun 1, 2023
+                    "tailwindcss",     -- associates with too many filetypes
+                    "biome",           -- not mature enough to be default
+                },
+                preferred_servers = {
+                    markdown = {},
+                    python = { "basedpyright", "ruff" },
                 },
                 configs = {
                     -- Special configuration for lua_ls
