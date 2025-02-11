28d1d5eaf99987a1f5d804b61c91def99f17ab6f
daniel <daniel.inama02@gmail.com>
2025-02-11 13:57:31 +0100

    fix hash

--- home/programs/shell/default.nix
+++ home/programs/shell/default.nix
@@ -165,7 +165,7 @@ in {
           owner = "Rolv-Apneseth";
           repo = "starship.yazi";
           rev = "d1cd0a38aa6a2c2e86e62a466f43e415f781031e";
-          hash = "sha256-L7MkZZqJ+t+A61ceC4Q1joLF6ytoWdgx9BwZWAGAoCA=";
+          hash = "sha256-XiEsykudwYmwSNDO41b5layP1DqVa89e6Emv9Qf0mz0=";
         };
       };
 
