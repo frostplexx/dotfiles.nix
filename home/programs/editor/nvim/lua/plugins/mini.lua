80f90df4589ec2cf5d28f111ad825e95824a5474
daniel <daniel.inama02@gmail.com>
2025-02-15 10:36:38 +0100

    enable man caches

--- home/programs/editor/nvim/lua/plugins/mini.lua
+++ home/programs/editor/nvim/lua/plugins/mini.lua
@@ -2,6 +2,7 @@ return {
     { -- Collection of various small independent plugins/modules
         'echasnovski/mini.nvim',
         event = "VeryLazy",
+        enabled = true,
         config = function()
             local function get_buffer_list()
                 local buffers = vim.fn.getbufinfo({ buflisted = 1 })
