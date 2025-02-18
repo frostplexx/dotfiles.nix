171a7bc681e59e7886cb9bd46c9a8437df291470
daniel <daniel.inama02@gmail.com>
2025-02-18 09:02:32 +0100

    stuff

--- home/programs/editor/nvim/lua/plugins/snacks.lua
+++ home/programs/editor/nvim/lua/plugins/snacks.lua
@@ -16,8 +16,57 @@ return {
         },
         debug = { enabled = true },
         input = { enabled = true },
-        image = { enabled = true },
-        notifier = { enabled = true },
+        image = {
+            formats = { 'png', 'jpg', 'jpeg', 'gif', 'bmp', 'webp', 'tiff', 'heic', 'avif', 'mp4', 'mov', 'avi', 'mkv', 'webm' },
+            force = false, -- try displaying the image, even if the terminal does not support it
+            markdown = {
+                -- enable image viewer for markdown files
+                -- if your env doesn't support unicode placeholders, this will be disabled
+                enabled = true,
+                max_width = 80,
+                max_height = 40,
+            },
+            -- window options applied to windows displaying image buffers
+            -- an image buffer is a buffer with `filetype=image`
+            wo = {
+                wrap = false,
+                number = false,
+                relativenumber = false,
+                cursorcolumn = false,
+                signcolumn = 'no',
+                foldcolumn = '0',
+                list = false,
+                spell = false,
+                statuscolumn = '',
+            },
+
+        },
+        notifier = {
+            enabled = true,
+            width = { min = 40, max = 0.4 },
+            height = { min = 1, max = 0.6 },
+            -- editor margin to keep free. tabline and statusline are taken into account automatically
+            margin = { top = 0, right = 1, bottom = 0 },
+            padding = true,              -- add 1 cell of left/right padding to the notification window
+            sort = { 'level', 'added' }, -- sort by level and time
+            -- minimum log level to display. TRACE is the lowest
+            -- all notifications are stored in history
+            level = vim.log.levels.TRACE,
+            icons = {
+                error = ' ',
+                warn = ' ',
+                info = ' ',
+                debug = ' ',
+                trace = ' ',
+            },
+            keep = function(_)
+                return vim.fn.getcmdpos() > 0
+            end,
+            style = 'fancy',
+            top_down = true,    -- place notifications from top to bottom
+            date_format = '%R', -- time format for notifications
+            refresh = 50,       -- refresh at most every 50ms
+        },
         quickfile = { enabled = true },
         statuscolumn = {
             enabled = true,
