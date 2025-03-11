d8857f5838acded5618f8de1af45d41a3ef151d0
daniel <daniel.inama02@gmail.com>
2025-03-11 09:41:04 +0100

    updates

--- home/programs/shell/default.nix
+++ home/programs/shell/default.nix
@@ -6,8 +6,8 @@
   yazi-plugins = pkgs.fetchFromGitHub {
     owner = "yazi-rs";
     repo = "plugins";
-    rev = "687f6f2a97ef7c691d623d910989f1a8dfdc0d7e";
-    hash = "sha256-tiJ/eDvUPqux33owjE4y5eLGJeT9GW4qQItPSC5i/nc=";
+    rev = "2bf70d880e02db95394de360668325b46f804791";
+    hash = "sha256-0A5UVbrP9+GRvX14VQm4Yxw+P9Ca5gtlk9qkLCVf5+Q=";
   };
 in {
   programs.zsh = {
