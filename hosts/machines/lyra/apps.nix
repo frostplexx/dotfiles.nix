6762f579775b0c8f733126503c524b284fad787b
daniel <daniel.inama02@gmail.com>
2025-03-17 10:02:09 +0100

    stuff

--- hosts/machines/lyra/apps.nix
+++ hosts/machines/lyra/apps.nix
@@ -33,20 +33,20 @@
     };
     masApps = {
       # Safari extensions
-      "1Password for Safari" = 1569813296;
-      "AdGuard for Safari" = 1440147259;
-      "SponsorBlock for Safari" = 1573461917;
-      "Raycast Companion" = 6738274497;
-      "Super Agent for Safari" = 1568262835;
-      "Noir" = 1592917505;
-      "Tailscale" = 1475387142;
-
-      "Xcode" = 497799835;
-      "Things" = 904280696;
-      "eduVPN" = 1317704208;
-      "Goodnotes" = 1444383602;
-      "WhatsApp" = 310633997;
-      "Windows App" = 1295203466;
+      # "1Password for Safari" = 1569813296;
+      # "AdGuard for Safari" = 1440147259;
+      # "SponsorBlock for Safari" = 1573461917;
+      # "Raycast Companion" = 6738274497;
+      # "Super Agent for Safari" = 1568262835;
+      # "Noir" = 1592917505;
+      # "Tailscale" = 1475387142;
+      #
+      # "Xcode" = 497799835;
+      # "Things" = 904280696;
+      # "eduVPN" = 1317704208;
+      # "Goodnotes" = 1444383602;
+      # "WhatsApp" = 310633997;
+      # "Windows App" = 1295203466;
     };
     taps = [
       "FelixKratz/formulae"
