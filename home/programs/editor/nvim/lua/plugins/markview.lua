69395b4d6a2746decd6550f6d01e8e772fa9e5fa
daniel <daniel.inama02@gmail.com>
2025-03-24 09:14:54 +0100

    remove markview

--- home/programs/editor/nvim/lua/plugins/markview.lua
+++
@@ -1,9 +0,0 @@
-return {
-    dir = "~/Developer/markview.nvim",
-    lazy = false,
-    opts = {
-        preview = {
-            icon_provider = "devicons", -- "mini" or "devicons"
-        }
-    }
-}
