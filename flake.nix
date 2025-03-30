154e07ce484ac771fd231ca502ee777751d3e3d2
daniel <daniel.inama02@gmail.com>
2025-03-30 17:08:39 +0200

    fixes

--- flake.nix
+++ flake.nix
@@ -10,9 +10,15 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-utils.url = "github:numtide/flake-utils";
+    lix = {
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
+    };
+
     lix-module = {
       url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.lix.follows = "lix";
     };
 
     # Darwin-specific inputs
