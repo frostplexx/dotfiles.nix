abe643be7a64296356728157d926360ba7eec86d
daniel <daniel.inama02@gmail.com>
2025-04-03 19:59:37 +0200

    update

--- machines/macbook-pro-m1/apps.nix
+++ machines/macbook-pro-m1/apps.nix
@@ -15,7 +15,24 @@
     zoom-us
     utm
     aerospace
+    # mac app store cli
+    mas
   ];
+
+  services.jankyborders = {
+    enable = true;
+    package = pkgs.jankyborders;
+
+    style = "round";
+    width = 3.0;
+    hidpi = true;
+    active_color = "0xffac8fd4";
+    inactive_color = "0xffac8fd4";
+    whitelist = [
+      "ghostty"
+      "kitty"
+    ];
+  };
 
   # Homebrew configuration
   homebrew = {
@@ -28,27 +45,26 @@
     };
     masApps = {
       # Safari extensions
-      "1Password for Safari" = 1569813296;
-      "AdGuard for Safari" = 1440147259;
-      "SponsorBlock for Safari" = 1573461917;
-      "Raycast Companion" = 6738274497;
-      "Super Agent for Safari" = 1568262835;
-      "Noir" = 1592917505;
-      "Tailscale" = 1475387142;
-      "Obsidian Web Clipper" = 6720708363;
+      # "1Password for Safari" = 1569813296;
+      # "AdGuard for Safari" = 1440147259;
+      # "SponsorBlock for Safari" = 1573461917;
+      # "Raycast Companion" = 6738274497;
+      # "Super Agent for Safari" = 1568262835;
+      # "Noir" = 1592917505;
+      # "Tailscale" = 1475387142;
+      # "Obsidian Web Clipper" = 6720708363;
       #
       # "Xcode" = 497799835;
-      "Things" = 904280696;
-      "eduVPN" = 1317704208;
-      "Goodnotes" = 1444383602;
-      "WhatsApp" = 310633997;
-      "Windows App" = 1295203466;
-      "Parcel" = 639968404;
+      # "Things" = 904280696;
+      # "eduVPN" = 1317704208;
+      # "Goodnotes" = 1444383602;
+      # "WhatsApp" = 310633997;
+      # "Windows App" = 1295203466;
+      # "Parcel" = 639968404;
     };
     # This doesn't work, taps are defined in flake.nix and then mksystem.nix
     # taps = [];
     brews = [
-      "mas"
     ];
     casks = [
       "altserver"
