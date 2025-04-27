df1525151925168ed6a2eff4bf830d60f09df785
daniel <daniel.inama02@gmail.com>
2025-04-27 10:30:23 +0200

    add automatic install

--- scripts/system-bootstrap.sh
+++ scripts/system-bootstrap.sh
@@ -5,7 +5,7 @@ source "$(dirname "$0")/colors.sh"
 
 function check_platform() {
     if [ "$(uname)" != "Darwin" ]; then
-        echo -e "${ERROR} Install is only supported on macOS"
+        echo -e "${ERROR} Bootstrap is only supported on macOS"
         exit 1
     fi
 }
@@ -13,20 +13,18 @@ function check_platform() {
 function install_nix() {
     if ! command -v nix >/dev/null 2>&1; then
         echo -e "${INFO} Installing Nix..."
-        #curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
         curl -sSf -L https://install.lix.systems/lix | sh -s -- install
+        export PATH="/run/current-system/sw/bin/:$PATH".
         echo -e "${SUCCESS} Nix installed successfully!"
-        echo -e "${WARN} Please restart your terminal and run 'make install' again."
-        exit 0
     else
-        echo -e "${INFO} Nix already installed"
+        echo -e "${WARN} Nix already installed"
     fi
 }
 
 
 function setup_nix_darwin() {
     if ! command -v darwin-rebuild > /dev/null 2>&1; then
-        echo -e "${INFO} Installing nix-darwin..."
+        echo -e "${INFO} Installing nix-darwin and deploying first generation..."
 
         local DARWIN_CONFIGS
         local config
@@ -36,16 +34,15 @@ function setup_nix_darwin() {
         nix run --extra-experimental-features "nix-command flakes" nixpkgs#nh -- darwin switch -H "$config" .
 
 
-        echo -e "${SUCCESS} nix-darwin installed successfully!"
+        echo -e "${SUCCESS} deployed successfully!"
     else
-        echo -e "${INFO} nix-darwin already installed"
+        echo -e "${WARN} nix-darwin already installed"
     fi
 }
 
 # Main script
-echo -e "${HEADER}Starting macOS Setup${RESET}"
+echo -e "${INFO}Starting macOS Setup${RESET}"
 check_platform
 install_nix
 setup_nix_darwin
-echo -e "${SUCCESS} Installation complete!"
-echo -e "${WARN} Please restart your shell and run './system-deploy.sh'\n"
+echo -e "${INFO} Installation complete!"
