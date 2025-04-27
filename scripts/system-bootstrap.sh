b579785674c5a55126d0f39272356bb0535d5496
Daniel Inama <62436912+frostplexx@users.noreply.github.com>
2025-04-27 20:28:27 +0200

    Update system-bootstrap.sh

--- scripts/system-bootstrap.sh
+++ scripts/system-bootstrap.sh
@@ -14,7 +14,7 @@ function install_nix() {
     if ! command -v nix >/dev/null 2>&1; then
         echo -e "${INFO} Installing Nix..."
         curl -sSf -L https://install.lix.systems/lix | sh -s -- install
-        export PATH="/run/current-system/sw/bin/:$PATH".
+        export PATH="/run/current-system/sw/bin/nix:$PATH".
         echo -e "${SUCCESS} Nix installed successfully!"
     else
         echo -e "${WARN} Nix already installed"
@@ -28,10 +28,10 @@ function setup_nix_darwin() {
 
         local DARWIN_CONFIGS
         local config
-        DARWIN_CONFIGS=$(nix eval --impure --json .#darwinConfigurations --apply builtins.attrNames 2>/dev/null || echo "[]")
+        DARWIN_CONFIGS=$(nix eval --impure --json "$HOME/dotfiles.nix"#darwinConfigurations --apply builtins.attrNames 2>/dev/null || echo "[]")
         config=$(echo "$DARWIN_CONFIGS" |nix run nixpkgs#jq -- -r '.[]' | nix run nixpkgs#fzf)
 
-        nix run --extra-experimental-features "nix-command flakes" nixpkgs#nh -- darwin switch -H "$config" .
+        nix run --extra-experimental-features "nix-command flakes" github:viperml/nh -- darwin switch -H "$config" "$HOME/dotfiles.nix"
 
 
         echo -e "${SUCCESS} deployed successfully!"
