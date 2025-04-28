945d3e7e20cecc1a8df66800598f1f4b7b4428da
daniel <daniel.inama02@gmail.com>
2025-04-28 12:36:41 +0200

    improve bootstrap script

--- scripts/system-bootstrap.sh
+++ scripts/system-bootstrap.sh
@@ -27,7 +27,6 @@ function install_nix() {
     fi
 }
 
-
 function setup_nix_darwin() {
     if ! command -v darwin-rebuild > /dev/null 2>&1; then
         echo -e "${INFO} Installing nix-darwin and deploying first generation..."
@@ -35,10 +34,27 @@ function setup_nix_darwin() {
         local DARWIN_CONFIGS
         local config
         DARWIN_CONFIGS=$(nix eval --impure --json "$HOME/dotfiles.nix"#darwinConfigurations --apply builtins.attrNames 2>/dev/null || echo "[]")
-        config=$(echo "$DARWIN_CONFIGS" |nix run nixpkgs#jq -- -r '.[]' | nix run nixpkgs#fzf)
+        
+        # Create the list of available configurations
+        configs=($(echo "$DARWIN_CONFIGS" | nix run nixpkgs#jq -- -r '.[]'))
+        
+        if [ ${#configs[@]} -eq 0 ]; then
+            echo -e "${ERROR} No configurations found."
+            exit 1
+        fi
 
+        # Display a numbered list and allow the user to choose
+        echo "Please select a configuration:"
+        select config in "${configs[@]}"; do
+            if [ -n "$config" ]; then
+                break
+            else
+                echo "Invalid choice, please try again."
+            fi
+        done < /dev/tty
+
+        # Deploy the chosen configuration
         nix run --extra-experimental-features "nix-command flakes" nixpkgs#nh -- darwin switch -H "$config" "$HOME/dotfiles.nix"
-
 
         echo -e "${SUCCESS} deployed successfully!"
     else
