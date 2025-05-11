cc7ff26c50e3118caafece648928f9d7d6c848a2
daniel <daniel.inama02@gmail.com>
2025-05-11 10:09:32 +0200

    remove some unused stuff

--- home/i3/rofi.nix
+++ home/i3/rofi.nix
@@ -16,14 +16,6 @@
         };
     };
     xdg.configFile = {
-        "rofi/catppuccin-mocha.rasi".source = builtins.fetchurl {
-            url = "https://raw.githubusercontent.com/catppuccin/rofi/refs/heads/main/themes/catppuccin-mocha.rasi";
-            sha256 = "0mdg3kmszkdr50n4c69p6xkf7ilh57h39hp4nwbfzyfm6wn89rza";
-        };
-        "rofi/catppuccin-default.rasi".source = builtins.fetchurl {
-            url = "https://raw.githubusercontent.com/catppuccin/rofi/refs/heads/main/catppuccin-default.rasi";
-            sha256 = "1lwzc71cdb7nd0qmmdnnkr512rdj6xk62mx260m049wi1vlg5pdv";
-        };
         "rofi/spotlight-theme.rasi".text = ''
             * {
                 background-color:      rgba(30, 30, 46, 80%);
