da2e5f4483d2a5cc5c8e439d0aabf8aca9a1bfd3
daniel <daniel.inama02@gmail.com>
2025-05-18 10:27:40 +0200

    updates

--- home/shell/fish/scripts/misc.fish
+++ home/shell/fish/scripts/misc.fish
@@ -1,3 +1,10 @@
+
+function clear_dns
+    sudo dscacheutil -flushcache; 
+    sudo killall -HUP mDNSResponder
+end
+
+
 function cocaine
     # Check current DPMS status
     set status (xset -q | grep 'DPMS is' | awk '{ print $3 }')
@@ -10,4 +17,8 @@ function cocaine
         xset +dpms
         dunstify 'Screen suspend is enabled.'
     end
+end
+
+function manf
+    /usr/bin/man -k . 2>/dev/null | fzf --preview 'man {1}' --preview-window=right:70%:wrap | awk '{print $1}' | xargs man
 end
