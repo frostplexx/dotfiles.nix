c08ab9c7fa8662d70ce3127e2c960d43d4a2078b
Daniel Inama <62436912+frostplexx@users.noreply.github.com>
2025-06-07 20:35:12 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -186,9 +186,18 @@ deploy_flake() {
         local NIXOS_CONFIGS
         local config
         NIXOS_CONFIGS=$(nix --extra-experimental-features nix-command --extra-experimental-features flakes eval --impure --json "$HOME/dotfiles.nix"#nixosConfigurations --apply builtins.attrNames 2>/dev/null || echo "[]")
-        config=$(echo "$NIXOS_CONFIGS" |nix --extra-experimental-features nix-command --extra-experimental-features flakes run --extra-experimental-features nix-command --extra-experimental-features flakes nixpkgs#jq -- -r '.[]' | nix run nixpkgs#fzf)
+        configs=("$(echo "$NIXOS_CONFIGS" | jq -r '.[]')")
 
-        print_status "Rebuilding NixOS configuration..."
+  	echo "Please select a configuration:"
+        select config in "${configs[@]}"; do
+            if [ -n "$config" ]; then
+                break
+            else
+                echo "Invalid choice, please try again."
+            fi
+        done < /dev/tty       
+	
+ 	print_status "Rebuilding NixOS configuration..."
         nixos-rebuild switch --flake "$HOME/dotfiles.nix#""$config" 
         
         print_header "INSTALLATION COMPLETE"
