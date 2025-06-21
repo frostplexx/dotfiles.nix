7421c78a0e9b046dccbc8dc5bcc49a1b62c5dee5
daniel <daniel.inama02@gmail.com>
2025-06-21 13:20:17 +0200

    fixes

--- machines/pc-nixos-gaming/default.nix
+++ machines/pc-nixos-gaming/default.nix
@@ -1,7 +1,7 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-{...}: {
+{ ...}: {
     imports = [
         # Include the results of the hardware scan.
         ./hardware-configuration.nix
@@ -43,6 +43,27 @@
     };
 
     environment.pathsToLink = ["/libexec"];
+    services = {
+        xserver = {
+            enable = true;
+
+            # Configure mouse settings to disable acceleration
+            libinput = {
+                enable = true;
+                mouse = {
+                    accelProfile = "flat";
+                    accelSpeed = "0";
+                    middleEmulation = false;
+                };
+            };
+
+            videoDrivers = ["nvidia"];
+            xkb = {
+                layout = "us";
+                variant = "";
+            };
+        };
+    };
 
     # Define a user account. Don't forget to set a password with ‘passwd’.
     users = {
