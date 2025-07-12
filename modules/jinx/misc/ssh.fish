ff0f314c9c53baf0d0f38ddf9d2c51f657a689f2
daniel <daniel.inama02@gmail.com>
2025-07-12 17:17:23 +0200

    chore: refactor jinx and move it to modules

---
+++ modules/jinx/misc/ssh.fish
@@ -0,0 +1,38 @@
+#!/usr/bin/env fish
+
+set -g ssh_hosts_file ~/.ssh/hosts
+
+function _create_hosts_file -a name url
+    # Remove ssh:// prefix
+    set -l cleaned (string replace -r '^ssh://' '' $url)
+
+    # Split into user and hostname
+    set -l user_host (string split '@' $cleaned)
+    set -l user $user_host[1]
+    set -l host $user_host[2]
+
+    echo "Host $name" >>$ssh_hosts_file
+    echo "    HostName \"$host\"" >>$ssh_hosts_file
+    echo "    User $user" >>$ssh_hosts_file
+    echo "" >>$ssh_hosts_file
+end
+
+function generate_ssh_hosts
+    set -l items (op item list --tags ssh --format json | jq -r '.[] | @base64')
+
+    # Clear previous content
+    echo -n "" >$ssh_hosts_file
+
+    echo "Generating..."
+    for item in $items
+        set -l _item (echo $item | base64 --decode | jq -r '.id, .title')
+        set -l id $_item[1]
+        set -l name $_item[2]
+        set -l url (op item get $id --fields label=url)
+        _create_hosts_file $name $url
+    end
+
+    echo Done
+end
+
+generate_ssh_hosts
