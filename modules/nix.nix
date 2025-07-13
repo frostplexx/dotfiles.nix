c4b4c48db074e8abad2ff30c778a3299e7f1a36d
daniel <daniel.inama02@gmail.com>
2025-07-13 09:36:51 +0200

    fix: fix cache

--- modules/nix.nix
+++ modules/nix.nix
@@ -9,7 +9,10 @@
         lazy-trees = true
         warn-dirty = false
         substituters = https://nix-community.cachix.org https://cache.nixos.org
-        trusted-public-keys = nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM=
+        trusted-public-keys = nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
+
+        auto-optimise-store = true
+        max-jobs = auto
     '';
 
     # Determinate uses its own daemon to manage the Nix installation that
