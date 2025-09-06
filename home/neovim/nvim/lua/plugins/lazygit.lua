d51047ba8453a2918e9305607d7fd003864b6e35
daniel <daniel.inama02@gmail.com>
2025-09-06 16:06:00 +0200

    chore: remove broder from lazygit

--- home/neovim/nvim/lua/plugins/lazygit.lua
+++ home/neovim/nvim/lua/plugins/lazygit.lua
@@ -5,7 +5,7 @@ return {
         { src = "https://github.com/nvim-lua/plenary.nvim" }
     },
     config = function()
-        -- Keymaps
+        vim.g.lazygit_floating_window_border_chars = { '', '', '', '', '', '', '', '' } -- remove window border
         vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>", { desc = "LazyGit" })
     end
 }
