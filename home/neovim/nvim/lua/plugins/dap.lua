a33c9c9852c0c82c33f23c7418c306b172a56177
daniel <daniel.inama02@gmail.com>
2025-09-06 16:10:35 +0200

    fix: add comment with key mappingas

--- home/neovim/nvim/lua/plugins/dap.lua
+++ home/neovim/nvim/lua/plugins/dap.lua
@@ -34,5 +34,16 @@ return {
                 args = { '-p', 'lldb', '--run', 'lldb-vscode' },
             }
         }
+
+        -- { "<leader>dc", "<cmd>DapContinue<CR>",                                       desc = "Debug Continue" },
+        -- { "<leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<CR>",              desc = "Debug Toggle Breakpoint" },
+        -- { "<leader>dn", "<cmd>DapStepOver<CR>",                                       desc = "Debug Step Over" },
+        -- { "<leader>di", "<cmd>DapStepInto<CR>",                                       desc = "Debug Step Into" },
+        -- { "<leader>do", "<cmd>DapStepOut<CR>",                                        desc = "Debug Step Out" },
+        -- { "<leader>dd", "<cmd>lua require'dap'.down()<CR>",                           desc = "Debug Down" },
+        -- { "<leader>ds", "<cmd>DapTerminate<CR>",                                      desc = "Debug Stop" },
+        -- { "<leader>dt", "<cmd>:lua require('dapui').toggle()<CR>",                    desc = "Debug Toggle Debug UI" },
+        -- { "<leader>da", "<cmd>DapNew<CR>",                                            desc = "Debug New" },
+        -- { "<leader>?",  "<cmd>:lua require('dapui').eval(nil, { enter = true })<CR>", desc = "Debug Eval" },
     end
 }
