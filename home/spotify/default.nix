5cddcaef88b53633d31c8593c11ccc6db59e087f
daniel <daniel.inama02@gmail.com>
2025-12-14 09:42:11 +0100

    chore: update flake inputs

--- home/spotify/default.nix
+++ home/spotify/default.nix
@@ -4,9 +4,21 @@
   ...
 }: let
   spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.system};
+
+  spotify = pkgs.spotify.overrideAttrs (oldAttrs: {
+    src =
+      if (pkgs.stdenv.isDarwin && pkgs.stdenv.isAarch64)
+      then
+        pkgs.fetchurl {
+          url = "https://web.archive.org/web/20251029235406/https://download.scdn.co/SpotifyARM64.dmg";
+          sha256 = "sha256-0gwoptqLBJBM0qJQ+dGAZdCD6WXzDJEs0BfOxz7f2nQ=";
+        }
+      else oldAttrs.src;
+  });
 in {
   programs.spicetify = {
     enable = true;
+    spotifyPackage = spotify;
     spotifyLaunchFlags = "--disable-update-restarts";
     enabledExtensions = with spicePkgs.extensions; [
       hidePodcasts
@@ -18,7 +30,9 @@ in {
     # enabledCustomApps = with spicePkgs.apps; [
     # ];
     # theme = spicePkgs.themes.comfy;
-    theme = spicePkgs.themes.catppuccin;
-    colorScheme = "mocha";
+    # theme = spicePkgs.themes.catppuccin;
+    theme = spicePkgs.themes.dribbblish;
+    # colorScheme = "mocha";
+    colorScheme = "catppuccin-mocha";
   };
 }
