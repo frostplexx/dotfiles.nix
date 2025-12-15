bb2f051176654cfa072d2833fc8f817492afc543
daniel <daniel.inama02@gmail.com>
2025-12-15 15:05:11 +0100

    refactor

--- home/neovim/neovim.nix
+++ home/neovim/neovim.nix
@@ -2,7 +2,12 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  langaugesConfig = import ./languages.nix {};
+  keymapsConfig = import ./keymap.nix {};
+  optionsConfig = import ./options.nix {};
+  # autocmdsConfig = import ./autocmds.nix {inherit lib;};
+in {
   programs.nvf = {
     enable = true;
     settings = {
@@ -10,20 +15,6 @@
         lazy.enable = false;
         viAlias = true;
         vimAlias = true;
-
-        options = {
-          clipboard = "unnamedplus";
-          tabstop = 4;
-          softtabstop = 4;
-          shiftwidth = 4;
-          expandtab = true;
-          smartindent = true;
-          fileencoding = "utf-8";
-          wrap = false;
-          swapfile = false;
-          backup = false;
-          undofile = true;
-        };
 
         lsp = {
           enable = true;
@@ -38,15 +29,13 @@
           };
         };
 
-        languages = {
-          enableTreesitter = true;
-          nix.enable = true;
-          ts.enable = true;
-          rust.enable = true;
-          python.enable = true;
-        };
+        inherit (optionsConfig) options;
+        inherit (langaugesConfig) languages;
+        inherit (keymapsConfig) keymaps;
+        # inherit (autocmdsConfig) autocmds;
 
         treesitter = {
+          enable = true;
           addDefaultGrammars = true;
           autotagHtml = true;
           grammars = pkgs.vimPlugins.nvim-treesitter.allGrammars;
@@ -138,6 +127,23 @@
           icons.enable = true;
           extra.enable = true;
 
+          starter = {
+            enable = true;
+            setupOpts = lib.generators.mkLuaInline ''
+              {
+                  items = {
+                      require("mini.starter").sections.builtin_actions(),
+                      { name = "Open Last File", action = "'0", section = "Builtin actions" }
+                  },
+                  content_hooks = {
+                      require("mini.starter").gen_hook.aligning('center', 'center'),
+                  },
+                  footer = "",
+                  silent = true,
+              }
+            '';
+          };
+
           pick = {
             enable = true;
             setupOpts = {
@@ -218,133 +224,6 @@
           };
           align.enable = true;
         };
-
-        keymaps = [
-          {
-            key = "<leader>go";
-            mode = "n";
-            noremap = false;
-            lua = true;
-            silent = true;
-            action = "function() MiniDiff.toggle_overlay() end";
-          }
-          {
-            key = "<leader>d";
-            mode = "n";
-            noremap = false;
-            lua = true;
-            silent = true;
-            action = "function() MiniBufremove.delete() end";
-          }
-          {
-            key = "<leader>f";
-            mode = "n";
-            silent = true;
-            noremap = false;
-            lua = true;
-            action = "function() MiniPick.builtin.files() end";
-          }
-          {
-            key = "<leader>gf";
-            mode = "n";
-            noremap = false;
-            lua = true;
-            silent = true;
-            action = "function() MiniPick.builtin.grep_live() end";
-          }
-          {
-            key = "<leader>ss";
-            mode = "n";
-            noremap = false;
-            lua = true;
-            silent = true;
-            action = "function() MiniExtra.pickers.lsp({ scope = 'workspace_symbol' }) end";
-          }
-          {
-            key = "<leader>tr";
-            mode = "n";
-            noremap = false;
-            lua = true;
-            silent = true;
-            action = "function() MiniExtra.pickers.diagnostic() end";
-          }
-          {
-            key = "<leader>bf";
-            mode = "n";
-            noremap = false;
-            lua = true;
-            silent = true;
-            action = "function() MiniPick.builtin.buffers() end";
-          }
-          {
-            key = "<leader>mk";
-            mode = "n";
-            noremap = false;
-            lua = true;
-            silent = true;
-            action = "function() MiniExtra.pickers.keymaps() end";
-          }
-          {
-            key = "<leader>ms";
-            mode = "n";
-            noremap = false;
-            lua = true;
-            silent = true;
-            action = "function() MiniExtra.pickers.marks() end";
-          }
-          {
-            key = "yc";
-            mode = "n";
-            noremap = false;
-            lua = true;
-            silent = true;
-            action = "function() vim.api.nvim_feedkeys('yygccp', 'm', false) end";
-            desc = "Duplicate a line and comment out the first line";
-          }
-          {
-            key = "<Esc>";
-            mode = "n";
-            silent = true;
-            action = "<cmd>nohlsearch<CR>";
-          }
-
-          {
-            key = "<c-u>";
-            mode = "n";
-            silent = true;
-            action = "<c-u>zz";
-            desc = "scroll up half a screen";
-          }
-          {
-            key = "<c-d>";
-            mode = "n";
-            silent = true;
-            action = "<c-d>zz";
-            desc = "scroll down half a screen";
-          }
-          {
-            key = "<leader>s";
-            mode = "v";
-            silent = true;
-            action = "y:%s/<C-r>/\"//gc<Left><Left><Left>";
-            desc = "Search and replace selected text across file";
-          }
-          {
-            key = "<leader>n";
-            mode = "n";
-            silent = true;
-            action = ":NoiceHistory<cr>";
-            desc = "Search and replace selected text across file";
-          }
-          {
-            key = "<leader>gg";
-            mode = "n";
-            silent = true;
-            lua = true;
-            action = "function() Snacks.lazygit() end";
-            desc = "Open Lazygit";
-          }
-        ];
       };
     };
   };
