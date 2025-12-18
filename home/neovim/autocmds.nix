083fa0e294e76ce6bcb119bd515b66e811b8d800
daniel <daniel.inama02@gmail.com>
2025-12-18 10:08:31 +0100

    fix?

--- home/neovim/autocmds.nix
+++ home/neovim/autocmds.nix
@@ -1,16 +1,16 @@
-_: {
+{lib, ...}: {
   autocmds = [
-    # {
-    #   group = "frostplexx/yank_highlight";
-    #   desc = "Highlight on yank";
-    #   event = ["TextYankPost"];
-    #   callback = lib.generators.mkLuaInline ''
-    #     function()
-    #           -- Setting a priority higher than the LSP references one.
-    #           vim.hl.on_yank { higroup = 'Visual', priority = 250 }
-    #       end
-    #   '';
-    # }
+    {
+      group = "frostplexx/yank_highlight";
+      desc = "Highlight on yank";
+      event = ["TextYankPost"];
+      callback = lib.generators.mkLuaInline ''
+        function()
+              -- Setting a priority higher than the LSP references one.
+              vim.hl.on_yank { higroup = 'Visual', priority = 250 }
+          end
+      '';
+    }
     # {
     #   event = ["FileType"];
     #   pattern = ["help"];
