3bae324395eee57c2371f8fbae17a87b7669ec37
daniel <daniel.inama02@gmail.com>
2026-01-16 10:22:18 +0100

    chore: improve AI commit in lazyvim

--- home/shell/lazygit.nix
+++ home/shell/lazygit.nix
@@ -19,14 +19,11 @@ _: {
           loadingText = "Generating commit messages...";
           prompts = [
             {
-              command = ''
-                /bin/bash -c "git diff HEAD | opencode run --model 'github-copilot/gemini-3-flash-preview' 'Generate a conventional commit title from the following git diff:' {}" '';
+              # command = ''/bin/bash -c "git diff HEAD | opencode run --model 'github-copilot/gemini-3-flash-preview' 'Generate a conventional commit title from the following git diff:' {}" '';
               key = "title";
-              type = "menuFromCommand";
+              type = "input";
+              suggestions.command = ''/bin/bash -c "git diff HEAD | opencode run --model 'github-copilot/gemini-3-flash-preview' 'Generate a conventional commit title from the following git diff:' {}" '';
               title = "AI Commit Message:";
-              # filter = "(?P<title>.+)";
-              # labelFormat = "{{.title}}";
-              loadingText = "Generating commit messages...";
             }
           ];
         }
@@ -47,19 +44,21 @@ _: {
           context = "localBranches";
           loadingText = "Checking out GitHub Pull Request...";
           command = "gh pr checkout {{.Form.PullRequestNumber}}";
-          prompt = {
-            type = "menuFromCommand";
-            title = "Which PR do you want to chekout?";
-            key = "PullRequestNumber";
-            command = ''
-              gh pr list --json number,title,headRefName,updatedAt
-            '';
-            filter = "#(?P<number>[0-9]+): (?P<title>.+) - (?P<ref_name>[^ ]+).*";
-            valueFormat = "{{.number}}";
-            labelFormat = ''
-              {{"#" | black | bold}}{{.number | white | bold}} {{.title | yellow | bold}}{{" [" | black | bold}}{{.ref_name | green}}{{"]" | black | bold}}
-            '';
-          };
+          prompts = [
+            {
+              type = "menuFromCommand";
+              title = "Which PR do you want to chekout?";
+              key = "PullRequestNumber";
+              command = ''
+                gh pr list --json number,title,headRefName,updatedAt --template '{{`{{range .}}{{printf "#%v: %s - %s (%s)" .number .title .headRefName (timeago .updatedAt)}}{{end}}`}}'
+              '';
+              filter = "#(?P<number>[0-9]+): (?P<title>.+) - (?P<ref_name>[^ ]+).*";
+              valueFormat = "{{.number}}";
+              labelFormat = ''
+                {{"#" | black | bold}}{{.number | white | bold}} {{.title | yellow | bold}}{{" [" | black | bold}}{{.ref_name | green}}{{"]" | black | bold}}
+              '';
+            }
+          ];
         }
       ];
     };
