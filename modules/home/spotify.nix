6ee5db0a728ea7f429c9523c221693d4b9beb4fd
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-02-13 12:08:59 +0100

    chore: update flake inputs

--- modules/home/spotify.nix
+++ modules/home/spotify.nix
@@ -1,21 +1,9 @@
 {inputs, ...}: {
   flake.modules.homeManager.spotify = {pkgs, ...}: let
     spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.system};
-
-    spotify = pkgs.spotify.overrideAttrs (oldAttrs: {
-      src =
-        if (pkgs.stdenv.isDarwin && pkgs.stdenv.isAarch64)
-        then
-          pkgs.fetchurl {
-            url = "https://web.archive.org/web/20251029235406/https://download.scdn.co/SpotifyARM64.dmg";
-            sha256 = "sha256-gEZxRBT7Jo2m6pirf+CreJiMeE2mhIkpe9Mv5t0RI58=";
-          }
-        else oldAttrs.src;
-    });
   in {
     programs.spicetify = {
       enable = true;
-      spotifyPackage = spotify;
       spotifyLaunchFlags = "--disable-update-restarts";
       enabledExtensions = with spicePkgs.extensions; [
         hidePodcasts
