3819207b4ba5cd5cf0aa6a01ad46f2a58bdb9df8
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-02-20 19:56:49 +0100

    fix?

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -1,42 +1,40 @@
 _: {
-  flake.modules.homeManager.hyprland =
-    {
-      inputs,
-      pkgs,
-      lib,
-      ...
-    }:
-    {
-      wayland.windowManager.hyprland = lib.mkIf pkgs.stdenv.isLinux {
-        enable = true;
-        package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-        portalPackage =
-          inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-        # Whether to enable XWayland
-        xwayland.enable = true;
+  flake.modules.homeManager.hyprland = {
+    inputs,
+    pkgs,
+    lib,
+    ...
+  }: {
+    wayland.windowManager.hyprland = lib.mkIf pkgs.stdenv.isLinux {
+      enable = true;
+      package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+      portalPackage =
+        inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+      # Whether to enable XWayland
+      xwayland.enable = true;
 
-        # Optional
-        # Whether to enable hyprland-session.target on hyprland startup
-        systemd.enable = true;
-        settings = {
-          "$mod" = "SUPER";
+      # Optional
+      # Whether to enable hyprland-session.target on hyprland startup
+      systemd.enable = true;
+      settings = {
+        "$mod" = "SUPER";
 
-          monitor = "DP-1, 2560x1080@144, 0x0, 1";
+        monitor = "DP-1, 2560x1080@144, 0x0, 1";
 
-          bind = [
-            # keybindings
-            "$mod, Return, exec kitty"
-            "$mod, d, exec rofi -show drun"
-            "$mod, Shift, q, killwindow"
-          ];
+        bind = [
+          # keybindings
+          "$mod, Enter, exec, kitty"
+          "$mod, d, exec, rofi -show drun"
+          "$mod, Shift, q, killwindow"
+        ];
 
-          bindm = [
-            # mouse movements
-            "$mod, mouse:272, movewindow"
-            "$mod, mouse:273, resizewindow"
-            "$mod ALT, mouse:272, resizewindow"
-          ];
-        };
+        bindm = [
+          # mouse movements
+          "$mod, mouse:272, movewindow"
+          "$mod, mouse:273, resizewindow"
+          "$mod ALT, mouse:272, resizewindow"
+        ];
       };
     };
+  };
 }
