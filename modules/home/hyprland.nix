bb6915962ef2a1d8b13a1ccaad4e7ff7edbfff39
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-02-20 20:28:59 +0100

    stuff

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -10,158 +10,61 @@ _: {
       accent_color = "cba6f7";
     in
     {
-      wayland.windowManager.hyprland = lib.mkIf pkgs.stdenv.isLinux {
-        enable = true;
-        package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-        portalPackage =
-          inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-        xwayland.enable = true;
-        systemd.enable = true;
-        settings = {
-          "$mod" = "SUPER";
+      xdg.configFile = lib.mkIf pkgs.stdenv.isLinux {
+        # Copy the filtered nvim configuration directory
+        "hypr" = {
+          source = ./hyprland/hypr;
+          recursive = true;
+        };
+        "rofi" = {
+          source = ./hyprland/rofi;
+          recursive = true;
+        };
+        "swaync" = {
+          source = ./hyprland/swaync;
+          recursive = true;
+        };
+        "waybar" = {
+          source = ./hyprland/swaync;
+          recursive = true;
+        };
+        "wlogout" = {
+          source = ./hyprland/swaync;
+          recursive = true;
+        };
+      };
 
-          monitor = "DP-1, 2560x1080@144, 0x0, 1";
-
-          input = {
-            follow_mouse = 1;
-            sensitivity = 0; # -1.0 - 1.0, 0 means no modification.
-            accel_profile = "flat";
-          };
-
-          general = {
-            gaps_in = 5;
-            gaps_out = 5;
-            border_size = 5;
-            layout = "dwindle";
-          };
-
-          decoration = {
-            active_opacity = 1.0;
-            inactive_opacity = 0.9;
-            fullscreen_opacity = 1.0;
-          };
-
-          bind = [
-            # keybindings
-            "$mod, Return, exec, kitty"
-            "$mod, d, exec, rofi -show drun"
-
-            # Launch apps
-            "SUPER CTRL ALT, d, exec, discord"
-            "SUPER CTRL ALT, m, exec, tidal"
-            "SUPER CTRL ALT, t, exec, kitty"
-            "SUPER CTRL ALT, b, exec, zen"
-
-            # Focus windows (hjkl)
-            "SUPER CTRL ALT, h, movefocus, l"
-            "SUPER CTRL ALT, j, movefocus, d"
-            "SUPER CTRL ALT, k, movefocus, u"
-            "SUPER CTRL ALT, l, movefocus, r"
-
-            # Fullscreen
-            "SUPER CTRL ALT, space, fullscreen"
-
-            # Minimize
-            # "SUPER, m, minimize"
-
-            # Switch to workspace
-            "SUPER CTRL ALT, 1, workspace, 1"
-            "SUPER CTRL ALT, 2, workspace, 2"
-            "SUPER CTRL ALT, 3, workspace, 3"
-            "SUPER CTRL ALT, 4, workspace, 4"
-            "SUPER CTRL ALT, 5, workspace, 5"
-            "SUPER CTRL ALT, 6, workspace, 6"
-
-            # Move window to workspace and follow
-            "SUPER CTRL ALT SHIFT, 1, movetoworkspace, 1"
-            "SUPER CTRL ALT SHIFT, 2, movetoworkspace, 2"
-            "SUPER CTRL ALT SHIFT, 3, movetoworkspace, 3"
-            "SUPER CTRL ALT SHIFT, 4, movetoworkspace, 4"
-            "SUPER CTRL ALT SHIFT, 5, movetoworkspace, 5"
-            "SUPER CTRL ALT SHIFT, 6, movetoworkspace, 6"
-
-            # Move windows (swap)
-            "SUPER CTRL ALT SHIFT, h, movewindow, l"
-            "SUPER CTRL ALT SHIFT, j, movewindow, d"
-            "SUPER CTRL ALT SHIFT, k, movewindow, u"
-            "SUPER CTRL ALT SHIFT, l, movewindow, r"
-
-            # Toggle float
-            "SUPER CTRL ALT, f, togglefloating"
-
-            # Focus back and forth
-            "ALT, tab, cyclenext"
-            "ALT SHIFT, tab, cyclenext, prev"
-
-            # Enter resize mode
-            "SUPER CTRL ALT, r, submap, resize"
-          ];
-
-          bindl = [
-            # Resize mode bindings
-            # "submap:resize, h, resizeactive, -20 0"
-            # "submap:resize, j, resizeactive, 0 20"
-            # "submap:resize, k, resizeactive, 0 -20"
-            # "submap:resize, l, resizeactive, 20 0"
-            # "submap:resize, escape, submap, reset"
-            # "submap:resize, Return, submap, reset"
-          ];
-
-          bindm = [
-            # mouse movements
-            "$mod, mouse:272, movewindow"
-            "$mod, mouse:273, resizewindow"
-            "$mod ALT, mouse:272, resizewindow"
-          ];
-
-          # windowrulev2 = [
-          #   # Floating rules
-          #   "title:^(Picture(-| )in(-| )Picture|PiP)$, floating, oncreatedr:1"
-          #   "subrole:^AXSystemDialog$, floating, oncreatedr:1"
-          #   "title:^kittyfloat$, floating, oncreatedr:1"
-          #   "title:^MiniPlayer$, floating, oncreatedr:1"
-          #
-          #   # Browser apps -> Workspace 1
-          #   "app:^Firefox$, workspace: 1"
-          #   "app:^Zen$, workspace: 1"
-          #
-          #   # Development apps -> Workspace 2
-          #   "app:^(IntelliJ IDEA|WezTerm|Ghostty|Termius|kitty)$, workspace: 2"
-          #
-          #   # Productivity apps -> Workspace 3
-          #   "app:^(GoodNotes|Obsidian|Things)$, workspace: 3"
-          #
-          #   # Communication apps -> Workspace 4
-          #   "app:^(Vesktop|zoom.us|Discord|Mail)$, workspace: 4"
-          #
-          #   # Music -> Workspace 5
-          #   "app:^(Spotify|Music|TIDAL)$, workspace: 5"
-          # ];
-
-          workspace = [
-            "1, persistent:true, monitor:DP-1, default:true"
-            "2, persistent:true, monitor:DP-1"
-            "3, persistent:true, monitor:DP-1"
-            "4, persistent:true, monitor:DP-1"
-            "5, persistent:true, monitor:DP-1"
-            "6, persistent:true, monitor:DP-1"
-          ];
+      programs = lib.mkIf pkgs.stdenv.isLinux {
+        rofi = {
+          enable = true;
+        };
+        wlogout = {
+          enable = true;
         };
 
-        extraConfig = ''
-          env = NIXOS_OZONE_WL, 1
-          env = NIXPKGS_ALLOW_UNFREE, 1
-          env = XDG_CURRENT_DESKTOP, Hyprland
-          env = XDG_SESSION_TYPE, wayland
-          env = XDG_SESSION_DESKTOP, Hyprland
-          env = GDK_BACKEND, wayland
-          env = CLUTTER_BACKEND, wayland
-          env = QT_QPA_PLATFORM, wayland
-          env = QT_WAYLAND_DISABLE_WINDOWDECORATION, 1
-          env = QT_AUTO_SCREEN_SCALE_FACTOR, 1
-          env = WLR_NO_HARDWARE_CURSORS,1
-          env = MOZ_ENABLE_WAYLAND, 1
-        '';
+        waybar.enable = true;
       };
+
+      services.swaync = lib.mkIf pkgs.stdenv.isLinux {
+        enable = true;
+      };
+
+      wayland.windowManager.hyprland = lib.mkIf pkgs.stdenv.isLinux {
+
+        # Whether to enable Hyprland wayland compositor
+        enable = true;
+        # The hyprland package to use
+        package = pkgs.hyprland;
+        # Whether to enable XWayland
+        xwayland.enable = true;
+
+        portalPackage =
+          inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+
+        # Optional
+        # Whether to enable hyprland-session.target on hyprland startup
+        systemd.enable = true;
+      };
+
     };
 }
--- modules/hosts/hl-vm-gpu.nix
+++ modules/hosts/hl-vm-gpu.nix
@@ -1,385 +1,335 @@
-{inputs, ...}: {
-  flake.modules.nixos.tiramisu = {
-    pkgs,
-    lib,
-    config,
-    defaults,
-    modulesPath,
-    ...
-  }: let
-    inherit (defaults) user;
-  in {
-    imports = [
-      (modulesPath + "/installer/scan/not-detected.nix")
-      inputs.disko.nixosModules.disko
-    ];
+{ inputs, ... }:
+{
+  flake.modules.nixos.tiramisu =
+    {
+      pkgs,
+      lib,
+      config,
+      defaults,
+      modulesPath,
+      ...
+    }:
+    let
+      inherit (defaults) user;
+    in
+    {
+      imports = [
+        (modulesPath + "/installer/scan/not-detected.nix")
+        inputs.disko.nixosModules.disko
+      ];
 
-    system.stateVersion = "25.11";
+      system.stateVersion = "25.11";
 
-    # Boot configuration
-    boot = {
-      loader = {
-        systemd-boot.enable = true;
-        efi.canTouchEfiVariables = true;
+      # Boot configuration
+      boot = {
+        loader = {
+          systemd-boot.enable = true;
+          efi.canTouchEfiVariables = true;
+        };
+        initrd.availableKernelModules = [
+          "nvme"
+          "xhci_pci"
+          "ahci"
+          "usb_storage"
+          "usbhid"
+          "sd_mod"
+        ];
+        initrd.kernelModules = [ ];
+        kernelPackages = pkgs.linuxPackages_latest;
+        kernelModules = [ "kvm-amd" ];
+        extraModulePackages = [ config.boot.kernelPackages.nvidiaPackages.stable ];
+        kernelParams = [
+          "acpi_enforce_resources=lax"
+          "amd_iommu=on"
+          "kvm.enable_virt_at_load=0"
+          "iommu=pt"
+          "zswap.enabled=1"
+          "default_hugepagesz=2M"
+          "hugepagesz=2M"
+          "hugepages=1024"
+          "transparent_hugepage=always"
+        ];
       };
-      initrd.availableKernelModules = [
-        "nvme"
-        "xhci_pci"
-        "ahci"
-        "usb_storage"
-        "usbhid"
-        "sd_mod"
-      ];
-      initrd.kernelModules = [];
-      kernelPackages = pkgs.linuxPackages_latest;
-      kernelModules = ["kvm-amd"];
-      extraModulePackages = [config.boot.kernelPackages.nvidiaPackages.stable];
-      kernelParams = [
-        "acpi_enforce_resources=lax"
-        "amd_iommu=on"
-        "kvm.enable_virt_at_load=0"
-        "iommu=pt"
-        "zswap.enabled=1"
-        "default_hugepagesz=2M"
-        "hugepagesz=2M"
-        "hugepages=1024"
-        "transparent_hugepage=always"
-      ];
-    };
 
-    disko.devices = {
-      disk = {
-        main = {
-          type = "disk";
-          device = "/dev/nvme0n1";
-          content = {
-            type = "gpt";
-            partitions = {
-              ESP = {
-                priority = 1;
-                name = "ESP";
-                start = "1M";
-                end = "512M";
-                type = "EF00";
-                content = {
-                  type = "filesystem";
-                  format = "vfat";
-                  mountpoint = "/boot";
-                  mountOptions = ["umask=0077"];
-                };
-              };
-              swap = {
-                size = "35G";
-                type = "8200";
-                content = {
-                  type = "swap";
-                };
-              };
-              root = {
-                size = "100%";
-                content = {
-                  type = "btrfs";
-                  extraArgs = ["-f"];
-                  subvolumes = {
-                    "/rootfs" = {
-                      mountpoint = "/";
-                    };
-                    "/home" = {
-                      mountOptions = ["compress=zstd"];
-                      mountpoint = "/home";
-                    };
-                    "/nix" = {
-                      mountOptions = [
-                        "compress=zstd"
-                        "noatime"
-                      ];
-                      mountpoint = "/nix";
-                    };
-                    "/persist" = {
-                      mountpoint = "/persist";
-                    };
+      disko.devices = {
+        disk = {
+          main = {
+            type = "disk";
+            device = "/dev/nvme0n1";
+            content = {
+              type = "gpt";
+              partitions = {
+                ESP = {
+                  priority = 1;
+                  name = "ESP";
+                  start = "1M";
+                  end = "512M";
+                  type = "EF00";
+                  content = {
+                    type = "filesystem";
+                    format = "vfat";
+                    mountpoint = "/boot";
+                    mountOptions = [ "umask=0077" ];
                   };
-                  mountpoint = "/partition-root";
+                };
+                swap = {
+                  size = "35G";
+                  type = "8200";
+                  content = {
+                    type = "swap";
+                  };
+                };
+                root = {
+                  size = "100%";
+                  content = {
+                    type = "btrfs";
+                    extraArgs = [ "-f" ];
+                    subvolumes = {
+                      "/rootfs" = {
+                        mountpoint = "/";
+                      };
+                      "/home" = {
+                        mountOptions = [ "compress=zstd" ];
+                        mountpoint = "/home";
+                      };
+                      "/nix" = {
+                        mountOptions = [
+                          "compress=zstd"
+                          "noatime"
+                        ];
+                        mountpoint = "/nix";
+                      };
+                      "/persist" = {
+                        mountpoint = "/persist";
+                      };
+                    };
+                    mountpoint = "/partition-root";
+                  };
                 };
               };
             };
           };
         };
       };
-    };
 
-    # Let disko handle fileSystems
-    # Manual fileSystems disabled - handled by disko
-    # fileSystems = lib.mkForce {};
-    # swapDevices = lib.mkForce [];
+      # Let disko handle fileSystems
+      # Manual fileSystems disabled - handled by disko
+      # fileSystems = lib.mkForce {};
+      # swapDevices = lib.mkForce [];
 
-    # Networking
-    networking = {
-      hostName = "tiramisu";
-      networkmanager.enable = true;
-      useDHCP = lib.mkDefault true;
-      interfaces."enp4s0".wakeOnLan.enable = true;
-    };
-
-    nixpkgs = {
-      hostPlatform = lib.mkDefault "x86_64-linux";
-      buildPlatform = lib.mkDefault "x86_64-linux";
-    };
-
-    # Nix settings
-    nix = {
-      channel.enable = false;
-      extraOptions = ''
-        experimental-features = nix-command flakes
-        warn-dirty = false
-      '';
-      nixPath = ["nixpkgs=${inputs.nixpkgs}"];
-      settings = {
-        extra-platforms = ["aarch64-darwin"];
-        extra-trusted-users = [user];
-        trusted-users = [
-          "root"
-          user
-        ];
-        max-jobs = 8;
-        sandbox = true;
-        substituters = [
-          "https://cache.nixos.org/"
-          "https://nix-community.cachix.org"
-          "https://ojsef39.cachix.org"
-        ];
-        trusted-public-keys = [
-          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-          "ojsef39.cachix.org-1:Pe8zOhPVMt4fa/2HYlquHkTnGX3EH7lC9xMyCA2zM3Y="
-        ];
+      # Networking
+      networking = {
+        hostName = "tiramisu";
+        networkmanager.enable = true;
+        useDHCP = lib.mkDefault true;
+        interfaces."enp4s0".wakeOnLan.enable = true;
       };
-    };
 
-    # Time and locale
-    time.timeZone = defaults.system.timeZone;
-    i18n.defaultLocale = defaults.system.locale;
-    i18n.extraLocaleSettings = {
-      LC_ADDRESS = "de_DE.UTF-8";
-      LC_IDENTIFICATION = "de_DE.UTF-8";
-      LC_MEASUREMENT = "de_DE.UTF-8";
-      LC_MONETARY = "de_DE.UTF-8";
-      LC_NAME = "de_DE.UTF-8";
-      LC_NUMERIC = "de_DE.UTF-8";
-      LC_PAPER = "de_DE.UTF-8";
-      LC_TELEPHONE = "de_DE.UTF-8";
-      LC_TIME = "de_DE.UTF-8";
-    };
+      nixpkgs = {
+        hostPlatform = lib.mkDefault "x86_64-linux";
+        buildPlatform = lib.mkDefault "x86_64-linux";
+      };
 
-    # Power management
-    powerManagement = {
-      enable = true;
-      cpuFreqGovernor = "performance";
-    };
-
-    environment.pathsToLink = [
-      "/share/applications"
-      "/share/xdg-desktop-portal"
-      "/libexec"
-    ];
-
-    # Services
-    services = {
-      gvfs.enable = true;
-      # xserver = {
-      #   enable = true;
-      #   videoDrivers = [ "nvidia" ];
-      #   xkb = {
-      #     layout = "us";
-      #     variant = "";
-      #   };
-      # };
-      libinput = {
-        enable = true;
-        mouse = {
-          accelProfile = "flat";
-          accelSpeed = "0";
-          middleEmulation = false;
+      # Nix settings
+      nix = {
+        channel.enable = false;
+        extraOptions = ''
+          experimental-features = nix-command flakes
+          warn-dirty = false
+        '';
+        nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
+        settings = {
+          extra-platforms = [ "aarch64-darwin" ];
+          extra-trusted-users = [ user ];
+          trusted-users = [
+            "root"
+            user
+          ];
+          max-jobs = 8;
+          sandbox = true;
+          substituters = [
+            "https://cache.nixos.org/"
+            "https://nix-community.cachix.org"
+            "https://ojsef39.cachix.org"
+          ];
+          trusted-public-keys = [
+            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            "ojsef39.cachix.org-1:Pe8zOhPVMt4fa/2HYlquHkTnGX3EH7lC9xMyCA2zM3Y="
+          ];
         };
       };
-      displayManager = {
-        ly.enable = true;
-        # autoLogin = {
-        #   enable = true;
-        #   inherit user;
-        # };
+
+      # Time and locale
+      time.timeZone = defaults.system.timeZone;
+      i18n.defaultLocale = defaults.system.locale;
+      i18n.extraLocaleSettings = {
+        LC_ADDRESS = "de_DE.UTF-8";
+        LC_IDENTIFICATION = "de_DE.UTF-8";
+        LC_MEASUREMENT = "de_DE.UTF-8";
+        LC_MONETARY = "de_DE.UTF-8";
+        LC_NAME = "de_DE.UTF-8";
+        LC_NUMERIC = "de_DE.UTF-8";
+        LC_PAPER = "de_DE.UTF-8";
+        LC_TELEPHONE = "de_DE.UTF-8";
+        LC_TIME = "de_DE.UTF-8";
       };
-      openssh.enable = true;
-      getty = {
-        autologinUser = user;
-        extraArgs = ["--noclear"];
-      };
-      pipewire = {
+
+      # Power management
+      powerManagement = {
         enable = true;
-        alsa = {
+        cpuFreqGovernor = "performance";
+      };
+
+      environment.pathsToLink = [
+        "/share/applications"
+        "/share/xdg-desktop-portal"
+        "/libexec"
+      ];
+
+      # Services
+      services = {
+        gvfs.enable = true;
+        # xserver = {
+        #   enable = true;
+        #   videoDrivers = [ "nvidia" ];
+        #   xkb = {
+        #     layout = "us";
+        #     variant = "";
+        #   };
+        # };
+        libinput = {
+          enable = true;
+          mouse = {
+            accelProfile = "flat";
+            accelSpeed = "0";
+            middleEmulation = false;
+          };
+        };
+        displayManager = {
+          ly.enable = true;
+          # autoLogin = {
+          #   enable = true;
+          #   inherit user;
+          # };
+        };
+        openssh.enable = true;
+        getty = {
+          autologinUser = user;
+          extraArgs = [ "--noclear" ];
+        };
+        pipewire = {
+          enable = true;
+          alsa = {
+            enable = true;
+            # 32-bit support disabled due to nixpkgs-unstable issue
+            # support32Bit = true;
+          };
+          pulse.enable = true;
+        };
+        printing.enable = true;
+        sunshine = {
+          enable = true;
+          autoStart = true;
+          capSysAdmin = true;
+          openFirewall = true;
+        };
+      };
+
+      # Looking Glass
+      systemd.tmpfiles.rules = [
+        "f /dev/shm/looking-glass 0660 ${user} kvm -"
+      ];
+
+      # User configuration
+      programs.fish.enable = true;
+      users = {
+        defaultUserShell = pkgs.fish;
+        users.${user} = {
+          isNormalUser = true;
+          description = defaults.personalInfo.name;
+          initialPassword = "nixos";
+          extraGroups = [
+            "networkmanager"
+            "root"
+            "wheel"
+          ];
+        };
+      };
+
+      # Hardware
+      hardware = {
+        cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+        xone.enable = true;
+        graphics = {
           enable = true;
           # 32-bit support disabled due to nixpkgs-unstable issue
-          # support32Bit = true;
+          # enable32Bit = true;
         };
-        pulse.enable = true;
+        bluetooth = {
+          enable = true;
+          powerOnBoot = true;
+        };
+        nvidia = {
+          modesetting.enable = true;
+          powerManagement.enable = true;
+          open = false;
+          nvidiaSettings = true;
+          package = config.boot.kernelPackages.nvidiaPackages.latest;
+        };
       };
-      printing.enable = true;
-      sunshine = {
-        enable = true;
-        autoStart = true;
-        capSysAdmin = true;
-        openFirewall = true;
+
+      # Programs
+      programs = {
+        hyprland = {
+          enable = true;
+        };
+        steam = {
+          enable = true;
+          remotePlay.openFirewall = true;
+          dedicatedServer.openFirewall = true;
+          localNetworkGameTransfers.openFirewall = true;
+        };
+
+        _1password-gui = {
+          enable = true;
+          polkitPolicyOwners = [ user ];
+        };
       };
-    };
 
-    # Looking Glass
-    systemd.tmpfiles.rules = [
-      "f /dev/shm/looking-glass 0660 ${user} kvm -"
-    ];
-
-    # User configuration
-    programs.fish.enable = true;
-    users = {
-      defaultUserShell = pkgs.fish;
-      users.${user} = {
-        isNormalUser = true;
-        description = defaults.personalInfo.name;
-        initialPassword = "nixos";
-        extraGroups = [
-          "networkmanager"
-          "root"
-          "wheel"
+      # Environment
+      environment = {
+        systemPackages = with pkgs; [
         ];
-      };
-    };
-
-    # Hardware
-    hardware = {
-      cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-      xone.enable = true;
-      graphics = {
-        enable = true;
-        # 32-bit support disabled due to nixpkgs-unstable issue
-        # enable32Bit = true;
-      };
-      bluetooth = {
-        enable = true;
-        powerOnBoot = true;
-      };
-      nvidia = {
-        modesetting.enable = true;
-        powerManagement.enable = true;
-        open = false;
-        nvidiaSettings = true;
-        package = config.boot.kernelPackages.nvidiaPackages.latest;
-      };
-    };
-
-    # Programs
-    programs = {
-      hyprland = {
-        enable = true;
-      };
-      # nh = {
-      #     enable = true;
-      #     clean.enable = true;
-      #     clean.extraArgs = "--keep-since 4d --keep 3";
-      #     flake = "/home/${user}/${defaults.paths.flake}/";
-      # };
-      # Gamescope and Steam disabled temporarily due to 32-bit support issues in nixpkgs-unstable
-      # gamescope = {
-      #     enable = true;
-      #     capSysNice = true;
-      # };
-      # steam = {
-      #     enable = true;
-      #     remotePlay.openFirewall = true;
-      #     dedicatedServer.openFirewall = true;
-      #     localNetworkGameTransfers.openFirewall = true;
-      # };
-      # _1password-gui = {
-      #     enable = true;
-      #     polkitPolicyOwners = [user];
-      # };
-      # thunar = {
-      #     enable = true;
-      #     plugins = with pkgs.xfce; [
-      #         thunar-archive-plugin
-      #         thunar-volman
-      #     ];
-      # };
-    };
-
-    # Environment
-    environment = {
-      systemPackages = with pkgs; [
-        # looking-glass-client
-        # steam
-        # steam-run
-        # lutris
-        # gamemode
-        # gamescope
-        # kbd
-        # mangohud
-        # prismlauncher
-        # solaar
-        # linuxKernel.packages.linux_zen.xone
-        # ckan
-        # openrgb-with-all-plugins
-        # _1password-gui
-        # papirus-icon-theme
-        # simplescreenrecorder
-        # kdePackages.filelight
-        # vlc
-        # xclip
-        # cifs-utils
-        # xorg.xrandr
-        # xorg.xinput
-        # picom
-        # xfce.thunar
-        # xfce.tumbler
-        # xfce.thunar-archive-plugin
-        # xfce.thunar-volman
-        # ffmpegthumbnailer
-        # gvfs
-        # flameshot
-        # polkit
-        # catppuccin-gtk
-        # catppuccin-cursors
-        # feh
-        # networkmanager
-        # imlib2
-        # libsecret
-      ];
-      variables = {
-        FREETYPE_PROPERTIES = "cff:no-stem-darkening=0 autofitter:no-stem-darkening=0";
-        QT_ENABLE_HIGHDPI_SCALING = "1";
-        QT_AUTO_SCREEN_SCALE_FACTOR = "0.9";
-      };
-    };
-
-    # Documentation
-    documentation = {
-      enable = true;
-      man = {
-        enable = true;
-        generateCaches = true;
-      };
-      dev.enable = true;
-    };
-
-    # Home Manager
-    home-manager.users.${user} = _: {
-      home = {
-        stateVersion = "23.11";
-        username = user;
-        homeDirectory = "/home/${user}";
-        sessionVariables = {
-          NH_FLAKE = "$HOME/${defaults.paths.flake}";
-          EDITOR = "nvim";
+        variables = {
+          FREETYPE_PROPERTIES = "cff:no-stem-darkening=0 autofitter:no-stem-darkening=0";
+          QT_ENABLE_HIGHDPI_SCALING = "1";
+          QT_AUTO_SCREEN_SCALE_FACTOR = "0.9";
         };
       };
-      programs.home-manager.enable = true;
+
+      # Documentation
+      documentation = {
+        enable = true;
+        man = {
+          enable = true;
+          generateCaches = true;
+        };
+        dev.enable = true;
+      };
+
+      # Home Manager
+      home-manager.users.${user} = _: {
+        home = {
+          stateVersion = "23.11";
+          username = user;
+          homeDirectory = "/home/${user}";
+          sessionVariables = {
+            NH_FLAKE = "$HOME/${defaults.paths.flake}";
+            EDITOR = "nvim";
+          };
+        };
+        programs.home-manager.enable = true;
+      };
     };
-  };
 }
