9337b7d379389c12bbf2c1959a7aa8ffb41ed784
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-02-24 09:11:33 +0100

    refactor(shell): remove yazi, lazygit, btop, fastfetch config to dedicated modules

---
+++ modules/home/yazi.nix
@@ -0,0 +1,109 @@
+_: {
+  flake.modules.homeManager.yazi = {pkgs, ...}: let
+    yazi-flavors = pkgs.fetchFromGitHub {
+      owner = "yazi-rs";
+      repo = "flavors";
+      rev = "9e053d0686a7d54a125d67bdd3aabaa5116d6e99";
+      hash = "sha256-B9b6T9/RkJDkehMC5/MxqnkjxWj5LZg4jehAn6aeamE=";
+    };
+  in {
+    # Terminal file manager
+    programs.yazi = {
+      enable = true;
+      enableFishIntegration = true;
+      shellWrapperName = "y";
+      package = pkgs.yazi.override {
+        _7zz = pkgs._7zz.override {
+          useUasm = pkgs.stdenv.isLinux;
+        };
+      };
+
+      settings = {
+        mgr = {
+          sort_by = "natural";
+          sort_sensitive = false;
+          sort_reverse = false;
+          sort_dir_first = true;
+          linemode = "mtime";
+          show_hidden = true;
+          show_symlink = true;
+        };
+        flavor = {
+          dark = "catppuccin-mocha";
+        };
+
+        tasks = {
+          micro_workers = 5;
+          macro_workers = 10;
+          bizarre_retry = 5;
+        };
+      };
+
+      plugins = {
+        starship = pkgs.fetchFromGitHub {
+          owner = "Rolv-Apneseth";
+          repo = "starship.yazi";
+          rev = "eca186171c5f2011ce62712f95f699308251c749";
+          hash = "sha256-xcz2+zepICZ3ji0Hm0SSUBSaEpabWUrIdG7JmxUl/ts=";
+        };
+      };
+      flavors = {
+        catppuccin-mocha = "${yazi-flavors}/catppuccin-mocha.yazi";
+      };
+
+      initLua = ''
+        require("starship"):setup({
+            -- Hide flags (such as filter, find and search). This is recommended for starship themes which
+            -- are intended to go across the entire width of the terminal.
+            hide_flags = false, -- Default: false
+            -- Whether to place flags after the starship prompt. False means the flags will be placed before the prompt.
+            flags_after_prompt = true, -- Default: true
+            -- Custom starship configuration file to use
+            config_file = "~/.config/starship_full.toml", -- Default: nil
+        })
+      '';
+
+      keymap = {
+        mgr.prepend_keymap = [
+          {
+            on = "f";
+            run = "plugin jump-to-char";
+            desc = "Jump to char";
+          }
+          {
+            on = "T";
+            run = "plugin max-preview";
+            desc = "Maximize or restore the preview pane";
+          }
+          {
+            on = [
+              "c"
+              "h"
+            ];
+            run = "plugin chmod";
+            desc = "Chmod on selected files";
+          }
+          {
+            on = [
+              "g"
+              "i"
+            ];
+            run = "cd '~/Library/Mobile Documents/com~apple~CloudDocs'";
+            desc = "Go to iCloud";
+          }
+          {
+            on = "F";
+            run = "plugin smart-filter";
+            desc = "Toggle smart filter";
+          }
+          {
+            on = "<C-p>";
+            run = ''
+              shell 'qlmanage -p "$@"' --confirm
+            '';
+          }
+        ];
+      };
+    };
+  };
+}
