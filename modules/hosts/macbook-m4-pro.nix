3dfe89faa6b3b13ba6b554aafe521fc769df0dba
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-03-06 13:52:14 +0100

    feat: new update script

--- modules/hosts/macbook-m4-pro.nix
+++ modules/hosts/macbook-m4-pro.nix
@@ -18,8 +18,8 @@
       experimental-features = "nix-command flakes parallel-eval impure-derivations";
       lazy-trees = true;
       warn-dirty = false;
-      substituters = "https://nix-community.cachix.org https://cache.nixos.org";
-      trusted-public-keys = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=";
+      substituters = "https://frostplexx.cachix.org https://nix-community.cachix.org https://cache.nixos.org";
+      trusted-public-keys = "frostplexx.cachix.org-1:kjkhnGNSkUvf5Mx8OEfhzaR830CUkDRglaKduAcr3UQ= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=";
       extra-trusted-users = "root ${user}";
       eval-cores = 0;
       auto-optimise-store = true;
