578be8f2d5c363bb5ee10e528f305a23ae4878f4
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-03-29 10:30:45 +0200

    fixes

--- modules/home/tidal.nix
+++ modules/home/tidal.nix
@@ -9,44 +9,44 @@ _: {
 
       #To list settingsNames and settings:  const idb = await luna.core.ReactiveStore.getStore("@luna/pluginStorage").dump(); console.log(JSON.stringify(idb, null, 2));
       plugins = [
-        {
-          shortURL = "@meowarex/radiant-lyrics";
-          settingsName = "RadiantLyrics";
-          settings = {
-            "lyricsGlowEnabled" = false;
-            "trackTitleGlow" = false;
-            "hideUIEnabled" = false;
-            "playerBarVisible" = false;
-            "qualityProgressColor" = false;
-            "floatingPlayerBar" = true;
-            "playerBarTint" = 10;
-            "playerBarTintColor" = "#000000";
-            "playerBarTintCustomColors" = [];
-            "playerBarRadius" = 8;
-            "playerBarSpacing" = 10;
-            "CoverEverywhere" = false;
-            "performanceMode" = false;
-            "spinningArt" = true;
-            "textGlow" = 20;
-            "backgroundScale" = 15;
-            "backgroundRadius" = 25;
-            "backgroundContrast" = 120;
-            "backgroundBlur" = 80;
-            "backgroundBrightness" = 40;
-            "spinSpeed" = 45;
-            "settingsAffectNowPlaying" = true;
-            "stickyLyrics" = false;
-            "stickyLyricsIcon" = "sparkle";
-            "lyricsStyle" = 2;
-            "syllableStyle" = 0;
-            "contextAwareLyrics" = true;
-            "blurInactive" = true;
-            "bubbledLyrics" = true;
-            "syllableLogging" = false;
-            "lyricsFontSize" = 100;
-            "romanizeLyrics" = false;
-          };
-        }
+        # {
+        #   shortURL = "@meowarex/radiant-lyrics";
+        #   settingsName = "RadiantLyrics";
+        #   settings = {
+        #     "lyricsGlowEnabled" = false;
+        #     "trackTitleGlow" = false;
+        #     "hideUIEnabled" = false;
+        #     "playerBarVisible" = false;
+        #     "qualityProgressColor" = false;
+        #     "floatingPlayerBar" = true;
+        #     "playerBarTint" = 10;
+        #     "playerBarTintColor" = "#000000";
+        #     "playerBarTintCustomColors" = [];
+        #     "playerBarRadius" = 8;
+        #     "playerBarSpacing" = 10;
+        #     "CoverEverywhere" = false;
+        #     "performanceMode" = false;
+        #     "spinningArt" = true;
+        #     "textGlow" = 20;
+        #     "backgroundScale" = 15;
+        #     "backgroundRadius" = 25;
+        #     "backgroundContrast" = 120;
+        #     "backgroundBlur" = 80;
+        #     "backgroundBrightness" = 40;
+        #     "spinSpeed" = 45;
+        #     "settingsAffectNowPlaying" = true;
+        #     "stickyLyrics" = false;
+        #     "stickyLyricsIcon" = "sparkle";
+        #     "lyricsStyle" = 2;
+        #     "syllableStyle" = 0;
+        #     "contextAwareLyrics" = true;
+        #     "blurInactive" = true;
+        #     "bubbledLyrics" = true;
+        #     "syllableLogging" = false;
+        #     "lyricsFontSize" = 100;
+        #     "romanizeLyrics" = false;
+        #   };
+        # }
         {
           shortURL = "DiscordRPC";
           settingsName = "DiscordRPC";
