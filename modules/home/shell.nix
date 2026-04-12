cd0f9d4217e7123d266be1eef7df3e5f328fed57
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-04-12 14:52:45 +0200

    update

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -107,8 +107,8 @@ _: {
             src = pkgs.fetchFromGitHub {
               owner = "catppuccin";
               repo = "bat";
-              rev = "6810349b28055dce54076712fc05fc68da4b8ec0";
-              sha256 = "sha256-lJapSgRVENTrbmpVyn+UQabC9fpV1G1e+CdlJ090uvg=";
+              rev = "null";
+              sha256 = "";
             };
             file = "themes/Catppuccin Mocha.tmTheme";
           };
