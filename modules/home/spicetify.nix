75d6817666606a5cc598561fd45fae1256ae6225
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-06-28 08:31:03 +0200

    feat: stop spotify from auto updating

--- modules/home/spicetify.nix
+++ modules/home/spicetify.nix
@@ -2,6 +2,7 @@ _: {
   flake.homeManagerModules.spicetify = {
     pkgs,
     inputs,
+    lib,
     ...
   }: let
     spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
@@ -29,5 +30,17 @@ _: {
         fixedEpisodesIcon
       ];
     };
+
+    # Stop spotify from updating: https://github.com/NixOS/nixpkgs/issues/404502#issuecomment-3304356653
+    home.activation.disableSpotifyUpdates = lib.mkIf pkgs.stdenv.isDarwin (
+      lib.hm.dag.entryAfter ["writeBoundary"] ''
+        SPOTIFY_UPDATE_DIR=~/Library/Application\ Support/Spotify/PersistentCache/Update
+        if ! /usr/bin/stat -f "%Sf" "$SPOTIFY_UPDATE_DIR" 2> /dev/null | grep -q uchg; then
+          rm -rf "$SPOTIFY_UPDATE_DIR"
+          mkdir -p "$SPOTIFY_UPDATE_DIR"
+          /usr/bin/chflags uchg "$SPOTIFY_UPDATE_DIR"
+        fi
+      ''
+    );
   };
 }
