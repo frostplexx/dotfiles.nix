6b8badb672e10c59c970dea13e40e6b78e1a71e1
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-07-05 15:25:23 +0200

    update

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -12,6 +12,9 @@ _: {
       ];
       extraConfig = ''
         IdentityAgent = "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+
+        Match exec "echo %h | grep -qE '^10\.162\.233\.'"
+          ProxyJump sorbet
       '';
     };
   };
