965dde6001b5fc1a5fa65f9a7ad287a66116902e
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-07-10 08:45:26 +0200

    fix: repodex jump

--- modules/apps/repodex/jump.fish
+++ modules/apps/repodex/jump.fish
@@ -3,9 +3,11 @@
 set root $argv[1]
 set expanded_root (eval echo $root)
 
-set selected (fd -t d -p .git --max-depth 3 "$expanded_root" \
+set selected (fd -t f -t d --hidden -p '\.git/?$' --max-depth 4 "$expanded_root" \
+    | string replace -r '/\.git/*$' '' \
     | string replace "$expanded_root/" '' \
-    | sort \
+    | string trim --right --chars=/ \
+    | sort -u \
     | fzf \
         --prompt="  Jump to repo > " \
         --height=80% \
