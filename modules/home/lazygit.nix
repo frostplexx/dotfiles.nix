5f6dff92923ab1fc1146c23a574c33a8957aac00
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-07-26 16:25:35 +0200

    feat: make lazygit use catpuccin again

--- modules/home/lazygit.nix
+++ modules/home/lazygit.nix
@@ -1,5 +1,5 @@
 _: {
-  flake.homeManagerModules.lazygit = _: {
+  flake.homeManagerModules.lazygit = {defaults, ...}: {
     programs.lazygit = {
       enable = true;
       enableFishIntegration = true;
@@ -10,20 +10,40 @@ _: {
         gui = {
           border = "rounded";
           nerdFontsVersion = 3;
-          theme = {
-            activeBorderColor = ["#31748f" "bold"];
-            inactiveBorderColor = ["#6e6a86"];
-            searchingActiveBorderColor = ["#ebbcba" "bold"];
-            optionsTextColor = ["#9ccfd8"];
-            selectedLineBgColor = ["#31748f"];
-            inactiveViewSelectedLineBgColor = ["#26233a" "bold"];
-            cherryPickedCommitFgColor = ["#1f1d2e"];
-            cherryPickedCommitBgColor = ["#ebbcba"];
-            markedBaseCommitFgColor = ["#9ccfd8"];
-            markedBaseCommitBgColor = ["#f6c177"];
-            unstagedChangesColor = ["#eb6f92"];
-            defaultFgColor = ["#e0def4"];
-          };
+          theme =
+            {
+              "catppuccin" = {
+                activeBorderColor = ["#a6e3a1" "bold"];
+                inactiveBorderColor = ["#6c7086"];
+                searchingActiveBorderColor = ["#f5c2e7" "bold"];
+                optionsTextColor = ["#89b4fa"];
+                selectedLineBgColor = ["#585b70"];
+                inactiveViewSelectedLineBgColor = ["#45475a" "bold"];
+                cherryPickedCommitFgColor = ["#1e1e2e"];
+                cherryPickedCommitBgColor = ["#f5c2e7"];
+                markedBaseCommitFgColor = ["#89b4fa"];
+                markedBaseCommitBgColor = ["#fab387"];
+                unstagedChangesColor = ["#f38ba8"];
+                defaultFgColor = ["#cdd6f4"];
+              };
+              "rose-pine" = {
+                activeBorderColor = ["#31748f" "bold"];
+                inactiveBorderColor = ["#6e6a86"];
+                searchingActiveBorderColor = ["#ebbcba" "bold"];
+                optionsTextColor = ["#9ccfd8"];
+                selectedLineBgColor = ["#31748f"];
+                inactiveViewSelectedLineBgColor = ["#26233a" "bold"];
+                cherryPickedCommitFgColor = ["#1f1d2e"];
+                cherryPickedCommitBgColor = ["#ebbcba"];
+                markedBaseCommitFgColor = ["#9ccfd8"];
+                markedBaseCommitBgColor = ["#f6c177"];
+                unstagedChangesColor = ["#eb6f92"];
+                defaultFgColor = ["#e0def4"];
+              };
+            }
+            .${
+              defaults.settings.theme
+            };
         };
         customCommands = [
           # AI Commit using opencode
